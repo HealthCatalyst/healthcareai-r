0c34611db907ec9b2ef003bfb2f6968037befa93
HQCATALYST\changsu.lee <changsu.lee@healthcatalyst.com>
2016-09-08 14:30:14 -0600

    creating DeploySupervisedModel classes. TODO: updates on function bodies for setConfigs and loadData functions

---
+++ R/DeploySupervisedModelParameters.R
@@ -0,0 +1,34 @@
+# Import the common functions.
+library(R6)
+
+#' DeploySupervisedModelParameters class to set up parameters required to deploy SupervisedModel classes
+#'
+#' @description This step allows one to create deploy models on your data
+#' and helps determine which performs best.
+#' @docType class
+#' @importFrom R6 R6Class
+#' @references \url{http://products.healthcatalyst.com/Predictive}
+#' @seealso \code{\link{HCRTools}}
+#'
+#' @export
+
+DeploySupervisedModelParameters <- R6Class("DeploySupervisedModelParameters",
+
+  #Public members
+  public = list(
+
+    #parameters
+    df = NULL,
+    type = "",
+    grainCol = "",
+    predictedCol = NULL,
+    testWindowCol = NULL,
+    useSavedModel = NULL,
+    impute = FALSE,
+
+    #Constructor
+    initialize = function () {
+    }
+  )
+
+)
