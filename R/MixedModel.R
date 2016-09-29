1923c38fc45cee68eba74ef904f30a1226124d19
Levi Thatcher <levi.thatcher@healthcatalyst.com>
2016-09-29 10:28:03 -0600

    Cleaning extraneous code and adding in proper debugging options

--- R/MixedModel.R
+++ R/MixedModel.R
@@ -59,42 +59,40 @@ LinearMixedModel <- R6Class("LinearMixedModel",
 
     # Start of functions
     buildDataset = function(){
-      # Prepare data according to InTestWindow column
-      # 1) Combine test/train (which was randomly generated in base class)
-      print('Create combined test/train')
+      # TODO Soon: Prepare data according to InTestWindow column, in case
+      # user wants to predict for row that's not the last in the person group
+      # Combine test/train (which was randomly generated in base class)
+
       private$train_test <- rbind(private$dfTrain,private$dfTest)
-      print(str(private$train_test))
 
-      # 2) Order by PatientID, PatientEncounterID
-      print('Before ordering')
-      print('Heellllllooooo')
-      print('PredictedCol')
-      print(self$params$predictedCol)
-      print('Heellllllooooo2')
-      print('PersonalCol')
-      print(self$params$personCol)
-      print('GrainCol')
-      print(self$params$grainCol)
+      if (isTRUE(self$params$debug)) {
+        print('Re-combined train/test for MM specific use')
+        print(str(private$train_test))
+      }
 
-      #dd[with(dd, order(-z, b)), ]
-      private$train_test <- private$train_test[with(private$train_test,
-                                                    order(private$train_test[[self$params$grainCol]])), ]
-      print('Ordered by PatID, PatEncID')
-      print(str(private$train_test))
+      # TODO Later: figure out why orderering in sql query is better auc than internal
+      # ordering. http://stackoverflow.com/a/1296745/5636012
+      # If ordering using with, access PersonID col via df[[PersonID]]
 
-      # 3) Split out test/train using logic from deploy (ie with )
-      private$lmm_test <- setDT(private$train_test)[, .SD[.N], by = eval(self$params$personCol)]
+      # Split out test/train by taking last row of each PersonID for test set
+      # TODO Soon: do this split using the InTestWindowCol
       private$lmm_train <- setDT(private$train_test)[, .SD[1:.N-1], by = eval(self$params$personCol)]
-      print('Splitting for mixed test/train')
-      print(str(private$lmm_test))
-      print(str(private$lmm_train))
+      private$lmm_test <- setDT(private$train_test)[, .SD[.N], by = eval(self$params$personCol)]
+
+      if (isTRUE(self$params$debug)) {
+        print('Mixed model-specific training set after creation')
+        print(str(private$lmm_train))
+        print('Mixed model-specific test set after creation')
+        print(str(private$lmm_test))
+      }
+
     },
 
     # Override: build model
     # Linear Mixed model (random intercept with fixed mean)
     buildModel = function() {
 
-      # Build formula
+      # Start build formula by grabbing column names
       col_list <- colnames(private$lmm_train)
 
       # Remove target col from list
@@ -105,24 +103,25 @@ LinearMixedModel <- R6Class("LinearMixedModel",
 
       # Remove random-effects col from list
       fixed_cols_temp <- col_list[col_list != self$params$personCol]
-      print(fixed_cols_temp)
 
+      # Collapse columns in list into a large string of cols
       fixed_cols <- paste(fixed_cols_temp, "+ ", collapse = "")
-      print(fixed_cols)
 
       formula <- paste0(self$params$predictedCol, " ~ ",
                         fixed_cols,
                         "(1|", self$params$personCol, ")")
 
-      print(formula)
+      if (isTRUE(self$params$debug)) {
+        print('Formula to be used:')
+        print(formula)
+        print('Training the general linear mixed-model...')
+        print('Using random intercept with fixed mean...')
+      }
 
-      print(str(train))
-
-      # Train RandomForest
       if (self$params$type == 'classification') {
         private$fit.lmm = glmer(formula = formula,
                                 data = private$lmm_train,
-                                family = binomial(link='logit'))
+                                family = binomial(link = 'logit'))
       }
       else if (self$params$type == 'regression') {
         private$fit.lmm = glmer(formula = formula,
