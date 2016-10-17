3aa844e6da9ad30290ce0c86f2090d5eb9c16126
Levi Thatcher <levi.thatcher@healthcatalyst.com>
2016-10-17 12:08:38 -0600

    Changing from string to variables

--- CIBuild_DBCreate.ps1
+++ CIBuild_DBCreate.ps1
@@ -4,4 +4,4 @@ $sqlInstance = "(local)\SQL2012SP1"
 # attach mdf to local instance
 $mdfFile = "SAM.mdf"
 $ldfFile = "SAM_log.ldf"
-sqlcmd -S "$sqlInstance" -Q "Use [master]; CREATE DATABASE [$dbName] ON (FILENAME = '$mdfFile'),(FILENAME = '$ldfFile') for ATTACH"
+sqlcmd -S $sqlInstance -Q "Use [master]; CREATE DATABASE [$dbName] ON (FILENAME = $mdfFile),(FILENAME = $ldfFile) for ATTACH"
