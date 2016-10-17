18cf1b68e447707a3b2d121d26da8d9b7283f8fc
Levi Thatcher <levi.thatcher@healthcatalyst.com>
2016-10-17 11:16:17 -0600

    Moving mdf/ldf to package (temporarily) to create SAM db upon CI build

---
+++ CIBuild_DBCreate.ps1
@@ -0,0 +1,7 @@
+$dbName = "SAM"
+$sqlInstance = "(local)\SQL2012SP1"
+
+# attach mdf to local instance
+$mdfFile = "SAM.mdf"
+$ldfFile = "SAM_log.ldf"
+sqlcmd -S "$sqlInstance" -Q "Use [master]; CREATE DATABASE [$dbName] ON (FILENAME = '$mdfFile'),(FILENAME = '$ldfFile') for ATTACH"
--- DBCreate.ps1
+++
@@ -1,7 +0,0 @@
-$dbName = "SAM"
-$sqlInstance = "(local)\SQL2012SP1"
-
-# attach mdf to local instance
-$mdfFile = join-path $startPath "C:\Program Files\Microsoft SQL Server\MSSQL11.MSSQLSERVER\MSSQL\DATA\SAM.mdf"
-$ldfFile = join-path $startPath "C:\Program Files\Microsoft SQL Server\MSSQL11.MSSQLSERVER\MSSQL\DATA\SAM_log.ldf"
-sqlcmd -S "$sqlInstance" -Q "Use [master]; CREATE DATABASE [$dbName] ON (FILENAME = '$mdfFile'),(FILENAME = '$ldfFile') for ATTACH"
