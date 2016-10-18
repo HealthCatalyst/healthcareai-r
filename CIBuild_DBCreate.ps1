f684442597e5ce327f39eebfc435118a493534a7
Levi Thatcher <levi.thatcher@healthcatalyst.com>
2016-10-18 08:10:22 -0600

    Adding in project-specific path (within appveyor) to ldf/mdf

--- CIBuild_DBCreate.ps1
+++ CIBuild_DBCreate.ps1
@@ -1,15 +1,6 @@
-#$dbName = "SAM2"
-#$sqlInstance = "(local)\SQL2012SP1"
-#$sqlInstance = "HC1437"
-
-'hello'
-
 # attach mdf to local instance
 
-$mdfFile = "SAM.mdf"
+$mdfFile = "C:\projects\HCRTools\SAM.mdf"
+$ldfFile = "C:\projects\HCRTools\SAM_log.ldf"
 
-'hello2'
-$ldfFile = "SAM_log.ldf"
-
-'hello3'
 sqlcmd -S "(local)\SQL2012SP1" -Q "CREATE DATABASE [SAM2] ON (FILENAME = '$mdfFile'), (FILENAME = '$ldfFile') for ATTACH"
