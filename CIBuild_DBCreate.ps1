38762d2988f05ece93a600811a2edcc3c9246c40
Levi Thatcher <levi.thatcher@healthcatalyst.com>
2016-10-18 07:32:26 -0600

    sqlcmd syntax is now working

--- CIBuild_DBCreate.ps1
+++ CIBuild_DBCreate.ps1
@@ -1,7 +1,15 @@
-$dbName = "SAM"
-$sqlInstance = "(local)\SQL2012SP1"
+#$dbName = "SAM2"
+#$sqlInstance = "(local)\SQL2012SP1"
+#$sqlInstance = "HC1437"
+
+'hello'
 
 # attach mdf to local instance
-$mdfFile = "SAM.mdf"
-$ldfFile = "SAM_log.ldf"
-sqlcmd -S $sqlInstance -Q "Use [master]; CREATE DATABASE [$dbName] ON (FILENAME = $mdfFile),(FILENAME = $ldfFile) for ATTACH"
+
+#$mdfFile = "SAM.mdf"
+
+'hello2'
+#$ldfFile = "SAM_log.ldf"
+
+'hello3'
+sqlcmd -S "SQL2012SP1" -Q "CREATE DATABASE [SAM2] ON (FILENAME = 'SAM.mdf'), (FILENAME = 'SAM_log.ldf') for ATTACH"
