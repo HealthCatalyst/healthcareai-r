4d073f2248ea5bf0d699dc5750a7b6124fd51d2e
Levi Thatcher <levi.thatcher@healthcatalyst.com>
2016-10-18 09:41:08 -0600

    removing path within appveyor to mdf/ldf

--- CIBuild_DBCreate.ps1
+++ CIBuild_DBCreate.ps1
@@ -4,7 +4,7 @@ Push-AppveyorArtifact inst/CIDatabase/SAM.mdf
 Push-AppveyorArtifact inst/CIDatabase/SAM_log.ldf
 
 # Attach mdf to local instance
-$mdfFile = "C:\projects\HCRTools\SAM.mdf"
-$ldfFile = "C:\projects\HCRTools\SAM_log.ldf"
+$mdfFile = "SAM.mdf"
+$ldfFile = "SAM_log.ldf"
 
 sqlcmd -S "(local)\SQL2012SP1" -Q "CREATE DATABASE [SAM] ON (FILENAME = '$mdfFile'), (FILENAME = '$ldfFile') for ATTACH"
