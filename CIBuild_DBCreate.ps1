77d5e40427e69a15cc2d2ce25d4666a54511a022
Levi Thatcher <levi.thatcher@healthcatalyst.com>
2016-10-18 07:54:07 -0600

    moving full path to artifact grab

--- CIBuild_DBCreate.ps1
+++ CIBuild_DBCreate.ps1
@@ -6,10 +6,10 @@
 
 # attach mdf to local instance
 
-$mdfFile = "C:\Source\DataScience\HCRTools\SAM.mdf"
+$mdfFile = "SAM.mdf"
 
 'hello2'
-$ldfFile = "C:\Source\DataScience\HCRTools\SAM_log.ldf"
+$ldfFile = "SAM_log.ldf"
 
 'hello3'
 sqlcmd -S "(local)\SQL2012SP1" -Q "CREATE DATABASE [SAM2] ON (FILENAME = '$mdfFile'), (FILENAME = '$ldfFile') for ATTACH"
