dc6b65d54cfeb7915717b392d6411c81a17096dc
Levi Thatcher <levi.thatcher@healthcatalyst.com>
2016-10-18 09:15:06 -0600

    Uploading artifacts in yml

--- CIBuild_DBCreate.ps1
+++ CIBuild_DBCreate.ps1
@@ -1,12 +1,10 @@
 
 # Push artifacts up
-#Push-AppveyorArtifact SAM.mdf
-#Push-AppveyorArtifact SAM_log.ldf
-
-$env:APPVEYOR_BUILD_FOLDER="C:\projects\HCRTools\"
+Push-AppveyorArtifact SAM.mdf
+Push-AppveyorArtifact SAM_log.ldf
 
 # Attach mdf to local instance
 $mdfFile = "C:\projects\HCRTools\SAM.mdf"
 $ldfFile = "C:\projects\HCRTools\SAM_log.ldf"
 
-sqlcmd -S "(local)\SQL2012SP1" -Q "CREATE DATABASE [SAM2] ON (FILENAME = '$mdfFile'), (FILENAME = '$ldfFile') for ATTACH"
+sqlcmd -S "(local)\SQL2012SP1" -Q "CREATE DATABASE [SAM] ON (FILENAME = '$mdfFile'), (FILENAME = '$ldfFile') for ATTACH"
