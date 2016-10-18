efad35a867e12374a3eeab64411be21cd88d7df8
Levi Thatcher <levi.thatcher@healthcatalyst.com>
2016-10-18 10:10:44 -0600

    Now using fully qualified (backslash) path within appveyor

--- CIBuild_DBCreate.ps1
+++ CIBuild_DBCreate.ps1
@@ -4,7 +4,7 @@ Push-AppveyorArtifact inst/CIDatabase/SAM.mdf
 Push-AppveyorArtifact inst/CIDatabase/SAM_log.ldf
 
 # Attach mdf to local instance
-$mdfFile = "inst\CIDatabase\SAM.mdf"
-$ldfFile = "inst\CIDatabase\SAM_log.ldf"
+$mdfFile = "c:\projects\HCRTools\inst\CIDatabase\SAM.mdf"
+$ldfFile = "c:\projects\HCRTools\inst\CIDatabase\SAM_log.ldf"
 
 sqlcmd -S "(local)\SQL2012SP1" -Q "CREATE DATABASE [SAM] ON (FILENAME = '$mdfFile'), (FILENAME = '$ldfFile') for ATTACH"
