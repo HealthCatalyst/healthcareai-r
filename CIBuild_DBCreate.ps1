f94db49bbb126be7cc6bb97bad476643a9e39f7e
Levi Thatcher <levi.thatcher@healthcatalyst.com>
2016-10-18 07:50:46 -0600

    Adding full path to mdf/ldf

--- CIBuild_DBCreate.ps1
+++ CIBuild_DBCreate.ps1
@@ -6,10 +6,10 @@
 
 # attach mdf to local instance
 
-#$mdfFile = "SAM.mdf"
+$mdfFile = "C:\Source\DataScience\HCRTools\SAM.mdf"
 
 'hello2'
-#$ldfFile = "SAM_log.ldf"
+$ldfFile = "C:\Source\DataScience\HCRTools\SAM_log.ldf"
 
 'hello3'
-sqlcmd -S "(local)\SQL2012SP1" -Q "CREATE DATABASE [SAM2] ON (FILENAME = 'SAM.mdf'), (FILENAME = 'SAM_log.ldf') for ATTACH"
+sqlcmd -S "(local)\SQL2012SP1" -Q "CREATE DATABASE [SAM2] ON (FILENAME = '$mdfFile'), (FILENAME = '$ldfFile') for ATTACH"
