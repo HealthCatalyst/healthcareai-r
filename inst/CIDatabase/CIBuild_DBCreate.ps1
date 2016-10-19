308a211cdcdcf1c1694b8085ddec22930a7b0d3a
Levi Thatcher <levi.thatcher@healthcatalyst.com>
2016-10-19 08:07:38 -0600

    iterating through artifacts

--- inst/CIDatabase/CIBuild_DBCreate.ps1
+++ inst/CIDatabase/CIBuild_DBCreate.ps1
@@ -7,8 +7,12 @@
 #$mdfFile = "c:\projects\HCRTools\inst\CIDatabase\SAM.mdf"
 #$ldfFile = "c:\projects\HCRTools\inst\CIDatabase\SAM_log.ldf"
 
-$mdfFile = $artifacts[$SAM.mdf]
-$ldfFile = $artifacts[$SAM_log.ldf]
+foreach ($artifactName in $artifacts.keys) {
+  $artifacts[$artifactName]
+}
+
+$mdfFile = $artifacts[$SAM.mdf].path
+$ldfFile = $artifacts[$SAM_log.ldf].path
 
 sqlcmd -b -S "(local)\SQL2012SP1" -Q "CREATE DATABASE [SAM] ON (FILENAME = '$mdfFile'), (FILENAME = '$ldfFile') for ATTACH"
 
