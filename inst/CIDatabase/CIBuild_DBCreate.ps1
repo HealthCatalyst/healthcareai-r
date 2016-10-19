d0c29eb451d19e3574a6970e5c2ebd64c3bf688d
Levi Thatcher <levi.thatcher@healthcatalyst.com>
2016-10-19 07:42:44 -0600

    Adding artifact via UI

--- inst/CIDatabase/CIBuild_DBCreate.ps1
+++ inst/CIDatabase/CIBuild_DBCreate.ps1
@@ -1,17 +1,14 @@
 
 # Push artifacts up
-Push-AppveyorArtifact inst/CIDatabase/SAM.mdf
-Push-AppveyorArtifact inst/CIDatabase/SAM_log.ldf
+#Push-AppveyorArtifact inst/CIDatabase/SAM.mdf
+#Push-AppveyorArtifact inst/CIDatabase/SAM_log.ldf
 
 # Attach mdf to local instance
-$mdfFile = "c:\projects\HCRTools\inst\CIDatabase\SAM.mdf"
-$ldfFile = "c:\projects\HCRTools\inst\CIDatabase\SAM_log.ldf"
+#$mdfFile = "c:\projects\HCRTools\inst\CIDatabase\SAM.mdf"
+#$ldfFile = "c:\projects\HCRTools\inst\CIDatabase\SAM_log.ldf"
 
-@echo off
+$mdfFile = "c:\projects\HCRTools\SAM.mdf"
+$ldfFile = "c:\projects\HCRTools\SAM_log.ldf"
+
 sqlcmd -b -S "(local)\SQL2012SP1" -Q "CREATE DATABASE [SAM] ON (FILENAME = '$mdfFile'), (FILENAME = '$ldfFile') for ATTACH"
 
-if %ERRORLEVEL%==0 (
-  echo OK!
-) else (
-  echo ERROR=%ERRORLEVEL%
-)
