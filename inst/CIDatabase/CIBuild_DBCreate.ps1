f89d19f9a03320974502f89efe93a6a8e6f9d794
Levi Thatcher <levi.thatcher@healthcatalyst.com>
2016-10-19 08:41:21 -0600

    list artifacts uploaded via UI

--- inst/CIDatabase/CIBuild_DBCreate.ps1
+++ inst/CIDatabase/CIBuild_DBCreate.ps1
@@ -1,25 +1,28 @@
 
 # Push artifacts up
-Push-AppveyorArtifact inst/CIDatabase/SAM.mdf
-Push-AppveyorArtifact inst/CIDatabase/SAM_log.ldf
+#Push-AppveyorArtifact inst/CIDatabase/SAM.mdf
+#Push-AppveyorArtifact inst/CIDatabase/SAM_log.ldf
 
 #Push-AppveyorArtifact inst/CIDatabase/SAM2.mdf
 #Push-AppveyorArtifact inst/CIDatabase/SAM_log2.ldf
 
 # Attach mdf to local instance
-$mdfFile = "c:\projects\HCRTools\inst\CIDatabase\SAM.mdf"
-$ldfFile = "c:\projects\HCRTools\inst\CIDatabase\SAM_log.ldf"
+#$mdfFile = "c:\projects\HCRTools\inst\CIDatabase\SAM.mdf"
+#$ldfFile = "c:\projects\HCRTools\inst\CIDatabase\SAM_log.ldf"
 
-#foreach ($artifactName in $artifacts.keys) {
-#  $artifacts[$artifactName]
-#}
+foreach ($artifactName in $artifacts.keys) {
+  $artifacts[$artifactName]
+}
 
-#$mdfFile = $artifacts[$SAM.mdf].path
-#$ldfFile = $artifacts[$SAM_log.ldf].path
+$mdfFile = $artifacts[$SAM.mdf].path
+$ldfFile = $artifacts[$SAM_log.ldf].path
 
-sqlcmd -b -S "(local)\SQL2012SP1" -Q "CREATE DATABASE [SAM] ON (FILENAME = '$mdfFile'), (FILENAME = '$ldfFile') for ATTACH"
+$mdfFile
 
-Get-ChildItem -Path c:\projects\HCRTools\inst\CIDatabase
+#sqlcmd -b -S "(local)\SQL2012SP1" -Q "CREATE DATABASE [SAM] ON (FILENAME = '$mdfFile'), (FILENAME = '$ldfFile') for ATTACH"
+
+Get-ChildItem -Path c:\projects\HCRTools
+Get-ChildItem -Path c:\projects\
 
 #Copy-Item c:\projects\HCRTools\inst\CIDatabase\SAM2.mdf c:\projects\HCRTools\inst\CIDatabase\SAM.mdf
 #Copy-Item c:\projects\HCRTools\inst\CIDatabase\SAM_log2.ldf c:\projects\HCRTools\inst\CIDatabase\SAM_log.ldf
