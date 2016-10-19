c4adfaac37cf759db6aae42128700e6537055256
Levi Thatcher <levi.thatcher@healthcatalyst.com>
2016-10-19 09:28:50 -0600

    simplifying back to db create with only two artifacts (no duplicates)

--- inst/CIDatabase/CIBuild_DBCreate.ps1
+++ inst/CIDatabase/CIBuild_DBCreate.ps1
@@ -1,28 +1,15 @@
-
 # Push artifacts up
-#Push-AppveyorArtifact inst/CIDatabase/SAM.mdf
-#Push-AppveyorArtifact inst/CIDatabase/SAM_log.ldf
+Push-AppveyorArtifact inst/CIDatabase/SAM.mdf
+Push-AppveyorArtifact inst/CIDatabase/SAM_log.ldf
 
-#Push-AppveyorArtifact inst/CIDatabase/SAM2.mdf
-#Push-AppveyorArtifact inst/CIDatabase/SAM_log2.ldf
+# Use mdf/ldf to create SAM db
+$mdfFile = "c:\projects\HCRTools\inst\CIDatabase\SAM.mdf"
+$ldfFile = "c:\projects\HCRTools\inst\CIDatabase\SAM_log.ldf"
 
-# Attach mdf to local instance
-#$mdfFile = "c:\projects\HCRTools\inst\CIDatabase\SAM.mdf"
-#$ldfFile = "c:\projects\HCRTools\inst\CIDatabase\SAM_log.ldf"
+sqlcmd -b -S "(local)\SQL2012SP1" -Q "CREATE DATABASE [SAM] ON (FILENAME = '$mdfFile'), (FILENAME = '$ldfFile') for ATTACH"
 
-foreach ($artifactName in $artifacts.keys) {
-  $artifacts[$artifactName]
-}
-
-#$mdfFile = $artifacts[$SAM.mdf].path
-#$ldfFile = $artifacts[$SAM_log.ldf].path
-
-#$mdfFile
-
-#sqlcmd -b -S "(local)\SQL2012SP1" -Q "CREATE DATABASE [SAM] ON (FILENAME = '$mdfFile'), (FILENAME = '$ldfFile') for ATTACH"
-
+# Look at contents of a few relevant directories
+Get-ChildItem -Path c:\projects\HCRTools\inst\CIDatabase
 Get-ChildItem -Path c:\projects\HCRTools
 Get-ChildItem -Path c:\projects\
 
-#Copy-Item c:\projects\HCRTools\inst\CIDatabase\SAM2.mdf c:\projects\HCRTools\inst\CIDatabase\SAM.mdf
-#Copy-Item c:\projects\HCRTools\inst\CIDatabase\SAM_log2.ldf c:\projects\HCRTools\inst\CIDatabase\SAM_log.ldf
