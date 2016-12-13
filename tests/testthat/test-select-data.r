e14b58be6057a77fd41f1a1fa9a14c4acfeffd77
Levi Thatcher <levi.thatcher@healthcatalyst.com>
2016-12-13 14:06:10 -0700

    Removing warning messages from selectData for low-row-count queries

--- tests/testthat/test-select-data.r
+++ tests/testthat/test-select-data.r
@@ -12,44 +12,25 @@ SELECT TOP 10
 FROM [SAM].[dbo].[HCRDiabetesClinical] order by PatientEncounterID'
 
 query2 = '
-SELECT TOP 30
-      LDLNBR
-FROM [SAM].[dbo].[HCRDiabetesClinical] 
-WHERE LDLNBR is not NULL
-ORDER BY LDLNBR'
-
-query3 = '
 select top 0
        LDLNBR
 from [SAM].[dbo].[HCRDiabetesClinical] order by LDLNBR'
 
-query4 = '
+query3 = '
 SELECT [A1CNBR] FROM [SAM].[dbo].[DiabetesClinicall]'
 
 test_that("Returns correct selected data in data frame", {
-  expect_equal(capture.output(selectData(connection.string,query1)),
-               capture.output(rbind(cat('Too few rows returned from SQL: 10 rows returned.Adjust your query to return more data!'),
+  expect_equal(selectData(connection.string,query1),
                data.frame(SystolicBPNBR = c(167,153,170,187,188,
-                                            185,189,149,155,160)))))
-})
-
-test_that("Returns too few rows message when <200 rows 199 rows edge case", {
-  expect_equal(capture.output(selectData(connection.string,query2)),
-               capture.output(rbind(cat('Too few rows returned from SQL: 30 rows returned.Adjust your query to return more data!'),
-               data.frame(LDLNBR = c(71,71,72,72,73,
-                                    73,74,74,74,74,
-                                    75,75,75,75,76,
-                                    76,76,76,77,77,
-                                    77,78,78,78,78,
-                                    78,79,79,80,80)))))
+                                            185,189,149,155,160)))
 })
 
 test_that("Returns too few rows message when 0 rows edge case", {
-  expect_equal(capture.output(selectData(connection.string,query3)),
+  expect_equal(capture.output(selectData(connection.string,query2)),
                capture.output(cbind(cat('Too few rows returned from SQL: 0 rows returned.Adjust your query to return more data!'),data.frame(LDLNBR = character(),stringsAsFactors = FALSE))))
 })
 
 test_that("Returns SQL error message when SQL error", {
-  expect_error(capture.output(selectData(connection.string,query4)),
+  expect_error(capture.output(selectData(connection.string,query3)),
                'Your SQL contains an error.')
 })
