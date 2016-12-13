d5338ac5ad0b0cad06af3bb15d7a906a9caa4164
mmastand <michael.mastanduno@healthcatalyst.com>
2016-12-13 14:41:47 -0700

    Moved AUC function to common and deleted the standalone file.

--- R/common.R
+++ R/common.R
@@ -988,3 +988,62 @@ findBestAlternateScenarios <- function(dfAlternateFeat,
   dfOptResult
 }
 
+#' @title
+#' Generate ROC curve for a dataset.
+#' @description Generates ROC curve and AUC for Sensitivity/Specificity or 
+#' Precision/Recall.
+#' @param predictions A vector of predictions from a machine learning model.
+#' @param labels A vector of the true labels. Must be the same length as 
+#' predictions.
+#' @param aucType Can be "SS" or "PR". Defaults to SS.
+#' @param plotFlg Binary value controlling plots Defaults to FALSE (no).
+#' @return auc Integral AUC of ROC plot.
+#'
+#' @import ROCR
+#' @export
+#' @references \url{http://healthcare.ai}
+#' @seealso \code{\link{healthcareai}}
+#' @examples
+#' 
+
+generateAUC <- function(predictions, labels, aucType, plotFlg) {
+  # Error check for uneven length predictions and labels
+  if (length(predictions) != length(labels)) {
+    stop('Data vectors are not equal length!')
+  }
+  
+  # use SS if aucType is missing
+  if (missing(aucType)) {
+    aucType <- 'SS'
+  }
+  
+  # plot ROC curve if flag is missing
+  if (missing(plotFlg)) {
+    plotFlg <- FALSE
+  }
+  
+  # generate ROC data
+  roc1 <- prediction( predictions, labels)
+  
+  # get performance and AUC from either curve type
+  if (aucType == 'PR') {
+    # ROC Curve Precision/Recall
+    perf <- performance(roc1, "prec", "rec")
+    perf.auc <- performance(roc1, measure = "auc")
+    cat("Area under the curve: ", perf.auc@y.values[[1]])
+  }
+  # defaults to SS
+  else {
+    perf <- performance(roc1, "tpr","fpr")
+    perf.auc <- performance(roc1, measure = "auc")
+    cat("Area under the curve: ", perf.auc@y.values[[1]])
+  }
+  
+  # plot AUC 
+  if (plotFlg == TRUE){
+    plot(perf, col = "blue", lwd = 2)
+  }
+  
+  # return AUC
+  return(perf.auc@y.values[[1]])
+}
