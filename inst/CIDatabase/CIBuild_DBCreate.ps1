ea39b295bbfefe6e04532c6b233a8a933dfb64d3
mmastand <michael.mastanduno@healthcatalyst.com>
2017-01-09 09:59:28 -0700

    testing db

--- inst/CIDatabase/CIBuild_DBCreate.ps1
+++ inst/CIDatabase/CIBuild_DBCreate.ps1
@@ -4,7 +4,7 @@ Push-AppveyorArtifact inst/extdata/HCRDiabetesClinical.csv
 # Use csv file to create SAM db and populate tables
 $csvFile = "c:\projects\healthcareai-r\inst\extdata\HCRDiabetesClinical.csv"
 
-sqlcmd -b -S "(local)\SQL2012SP1" -Q "CREATE DATABASE [SAM]"
+sqlcmd -b -S "(local)\SQL2012SP1" -Q "CREATE DATABASE [SAM_test_for_R]"
 sqlcmd -S "(local)\SQL2012SP1" -Q "exec sp_databases"
 
 # Look at contents of a few relevant directories
