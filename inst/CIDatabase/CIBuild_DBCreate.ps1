1defdf63b52e6bcf765514e74ad7f31642123626
mmastand <michael.mastanduno@healthcatalyst.com>
2017-01-09 10:10:55 -0700

    trying to write tables

--- inst/CIDatabase/CIBuild_DBCreate.ps1
+++ inst/CIDatabase/CIBuild_DBCreate.ps1
@@ -7,6 +7,56 @@ $csvFile = "c:\projects\healthcareai-r\inst\extdata\HCRDiabetesClinical.csv"
 sqlcmd -b -S "(local)\SQL2012SP1" -Q "CREATE DATABASE [SAM_test_for_R]"
 sqlcmd -S "(local)\SQL2012SP1" -Q "exec sp_databases"
 
+# Write the tables
+sqlcmd -S "(local)\SQL2012SP1" -Q "
+CREATE TABLE [dbo].[HCRDeployClassificationBASE](
+    [BindingID] [int] NULL,
+    [BindingNM] [varchar](255) NULL,
+    [LastLoadDTS] [datetime2](7) NULL,
+    [PatientEncounterID] [decimal](38, 0) NULL,
+    [PredictedProbNBR] [decimal](38, 2) NULL,
+    [Factor1TXT] [varchar](255) NULL,
+    [Factor2TXT] [varchar](255) NULL,
+    [Factor3TXT] [varchar](255) NULL
+)
+
+CREATE TABLE [dbo].[HCRDeployRegressionBASE](
+    [BindingID] [int] NULL,
+    [BindingNM] [varchar](255) NULL,
+    [LastLoadDTS] [datetime2](7) NULL,
+    [PatientEncounterID] [decimal](38, 0) NULL,
+    [PredictedValueNBR] [decimal](38, 2) NULL,
+    [Factor1TXT] [varchar](255) NULL,
+    [Factor2TXT] [varchar](255) NULL,
+    [Factor3TXT] [varchar](255) NULL
+)
+
+CREATE TABLE [dbo].[HCRDiabetesClinical](
+    [PatientEncounterID] [float] NULL,
+    [PatientID] [float] NULL,
+    [SystolicBPNBR] [float] NULL,
+    [LDLNBR] [float] NULL,
+    [A1CNBR] [float] NULL,
+    [GenderFLG] [nvarchar](255) NULL,
+    [ThirtyDayReadmitFLG] [nvarchar](255) NULL,
+    [InTestWindowFLG] [nvarchar](255) NULL
+)
+BULK INSERT dbo.HCRDiabetesClinical
+WITH
+(
+    FIRSTROW = 2,
+    FIELDTERMINATOR = ',',  --CSV field delimiter
+    ROWTERMINATOR = '\n',   --Use to shift the control to next row
+    KEEPNULLS
+)
+
+CREATE TABLE [dbo].[HCRWriteData](
+    [a] [float] NULL,
+    [b] [float] NULL,
+    [c] [varchar](255) NULL
+)
+sqlcmd -S "(local)\SQL2012SP1" -Q "exec sp_tables"
+
 # Look at contents of a few relevant directories
 Get-ChildItem -Path c:\projects\healthcareai-r\inst\CIDatabase
 Get-ChildItem -Path c:\projects\healthcareai-r
