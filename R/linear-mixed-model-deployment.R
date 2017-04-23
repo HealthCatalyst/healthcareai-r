6d928242c05cc148806348bebfe251464324ba11
Levi Thatcher <levi.thatcher@healthcatalyst.com>
2017-04-23 15:54:09 -0600

    Adding csv example to LMM deploy

--- R/linear-mixed-model-deployment.R
+++ R/linear-mixed-model-deployment.R
@@ -34,8 +34,53 @@ source('R/supervised-model-deployment.R')
 #' F leads to removal of rows containing NULLs.
 #' @param debug Provides the user extended output to the console, in order
 #' to monitor the calculations throughout. Use T or F.
+#' @export
 #' @seealso \code{\link{healthcareai}}
 #' @examples
+#' 
+# #### Example using csv data ####
+#' ptm <- proc.time()
+#' library(healthcareai)
+#'
+#' # setwd('C:/Yourscriptlocation/Useforwardslashes') # Uncomment if using csv
+#' 
+#' # Can delete this line in your work
+#' csvfile <- system.file("extdata", 
+#'                        "HCRDiabetesClinical.csv", 
+#'                        package = "healthcareai")
+#'
+#' # Replace csvfile with 'path/file'
+#' df <- read.csv(file = csvfile, 
+#'                header = TRUE, 
+#'                na.strings = c("NULL", "NA", ""))
+#'
+#' head(df)
+#' str(df)
+#'
+#' p <- SupervisedModelDeploymentParams$new()
+#' p$type <- "classification"
+#' p$df <- df
+#' p$grainCol <- "PatientEncounterID"
+#' p$personCol <- "PatientID"
+#' p$testWindowCol <- "InTestWindowFLG"
+#' p$predictedCol <- "ThirtyDayReadmitFLG"
+#' p$impute <- TRUE
+#' p$debug <- FALSE
+#' p$useSavedModel <- FALSE
+#' p$cores <- 1
+#' p$writeToDB <- FALSE
+#'
+#' dLMM <- LinearMixedModelDeployment$new(p)
+#' dLMM$deploy()
+#' 
+#' df <- dLMM$getOutDf()
+#' # Write to CSV (or JSON, MySQL, etc) using R syntax
+#' # write.csv(df,'path/predictionsfile.csv')
+#' 
+#' print(proc.time() - ptm)
+#'
+#' \donttest{
+#'   
 #' #### Classification example using diabetes data ####
 #' # This example requires you to first create a table in SQL Server
 #' # If you prefer to not use SAMD, execute this in SSMS to create output table:
@@ -46,7 +91,6 @@ source('R/supervised-model-deployment.R')
 #' # Factor1TXT varchar(255), Factor2TXT varchar(255), Factor3TXT varchar(255)
 #' # )
 #'
-#' \donttest{
 #' # setwd('C:/Yourscriptlocation/Useforwardslashes') # Uncomment if using csv
 #' ptm <- proc.time()
 #' library(healthcareai)
@@ -75,14 +119,15 @@ source('R/supervised-model-deployment.R')
 #' df <- selectData(connection.string, query)
 #'
 #' head(df)
+#' str(df)
 #'
 #' p <- SupervisedModelDeploymentParams$new()
 #' p$type <- "classification"
 #' p$df <- df
 #' p$grainCol <- "PatientEncounterID"
+#' p$personCol <- "PatientID"
 #' p$testWindowCol <- "InTestWindowFLG"
 #' p$predictedCol <- "ThirtyDayReadmitFLG"
-#' p$personCol <- "PatientID"
 #' p$impute <- FALSE
 #' p$debug <- FALSE
 #' p$useSavedModel <- FALSE
@@ -94,6 +139,9 @@ source('R/supervised-model-deployment.R')
 #' lMM$deploy()
 #'
 #' print(proc.time() - ptm)
+#' }
+#' 
+#' \donttest{
 #'
 #' #### Regression example using diabetes data ####
 #' # This example requires you to first create a table in SQL Server
@@ -133,14 +181,15 @@ source('R/supervised-model-deployment.R')
 #' df <- selectData(connection.string, query)
 #'
 #' head(df)
+#' str(df)
 #'
 #' p <- SupervisedModelDeploymentParams$new()
 #' p$type <- "regression"
 #' p$df <- df
 #' p$grainCol <- "PatientEncounterID"
+#' p$personCol <- "PatientID"
 #' p$testWindowCol <- "InTestWindowFLG"
 #' p$predictedCol <- "A1CNBR"
-#' p$personCol <- "PatientID"
 #' p$impute <- TRUE
 #' p$debug <- FALSE
 #' p$useSavedModel <- FALSE
@@ -153,8 +202,6 @@ source('R/supervised-model-deployment.R')
 #'
 #' print(proc.time() - ptm)
 #' }
-#'
-#' @export
 
 LinearMixedModelDeployment <- R6Class("LinearMixedModelDeployment",
 
