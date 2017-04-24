8c4057537e981d6b70f8c7ff35066df3d804bb05
Levi Thatcher <levi.thatcher@healthcatalyst.com>
2017-04-24 09:52:33 -0600

    Switching message to cat or warning

--- R/common.R
+++ R/common.R
@@ -344,7 +344,7 @@ writeData <- function(df, server, database, schemaDotTable) {
 removeColsWithAllSameValue <- function(df) {
   dfResult <- df[sapply(df, function(x) length(unique(x[!is.na(x)])) > 1)]
   if (ncol(dfResult) == 0) {
-    message("All columns were removed.")
+    cat("All columns were removed.")
   }
   dfResult
 }
@@ -435,7 +435,7 @@ findTrends <- function(df, dateCol, groupbyCol) {
   }
 
   if (length(metricTrendList) == 0) {
-    message("No trends of sufficient length found")
+    cat("No trends of sufficient length found")
     return()
   } else {
     dfResult <- data.frame(groupbyCol, aggregatedColList, metricTrendList, finalYrMonth)
@@ -575,11 +575,13 @@ calculateAllCorrelations <- function(df) {
 #' @examples
 #' df <- data.frame(a=c(1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,
 #'                     1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1),
-#'                 b=c('a','b','c','d','e','f','g','h','i','j','k','l','m','n',
-#'                     'o','p','q','r','s','t','u','v','w','x','y','z','aa','bb',
-#'                     'cc','dd','ee','ff','gg','hh','ii','jj','kk','ll','mm','nn',
-#'                     'oo','pp','qq','rr','ss','tt','uu','vv','ww','xx','yy'))
+#'                  b=c('a','b','c','d','e','f','g','h','i','j','k','l','m','n',
+#'                      'o','p','q','r','s','t','u','v','w','x','y','z','aa',
+#'                      'bb','cc','dd','ee','ff','gg','hh','ii','jj','kk','ll',
+#'                      'mm','nn','oo','pp','qq','rr','ss','tt','uu','vv','ww',
+#'                      'xx','yy'))
 #' colList <- returnColsWithMoreThanFiftyCategories(df)
+#' colList
 
 returnColsWithMoreThanFiftyCategories <- function(df) {
   colList <- vector("character")
@@ -610,8 +612,8 @@ returnColsWithMoreThanFiftyCategories <- function(df) {
 #' colList
 
 countPercentEmpty <- function(df) {
-  message(paste0('This function has been deprecated and will be removed',
-                 ' after v0.1.13.\n',
+  warning(paste0('This function has been deprecated and will be removed',
+                 ' after v0.1.12.\n',
                  'Please instead see ?percentDataAvailableInDateRange.'))
   colList <- colMeans(is.na(df))
   colList
