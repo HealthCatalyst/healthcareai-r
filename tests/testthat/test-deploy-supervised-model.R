8f1b94ca4bc20931548b2d2a072dbc5426c44800
Levi Thatcher <levi.thatcher@healthcatalyst.com>
2017-05-08 21:38:37 -0600

    RF/Lasso unit tests (may work when dfOut is fixed)

--- tests/testthat/test-deploy-supervised-model.R
+++ tests/testthat/test-deploy-supervised-model.R
@@ -1,117 +1,140 @@
 context("Checking deploy supervised model")
 
-csvfile <- system.file("extdata",
-                       "HCRDiabetesClinical.csv",
-                       package = "healthcareai")
-
-df <- read.csv(file = csvfile,
-                    header = TRUE,
-                    na.strings =  c('NULL', 'NA', ""))
-
-connection.string <- 'driver={SQL Server};
-                      server=localhost;
-                      database=SAM;
-                      trusted_connection=true'
-
-df$PatientID <- NULL
-
-test_that("rf predicted val (with spec mtry) is the same each time", {
+test_that("rf predicted val (w/out mtry tuning) is the same each time", {
   skip_on_travis()
   skip_on_cran()
-  p <- SupervisedModelDeploymentParams$new()
-  p$type = 'classification'
-  p$df = df
-  p$grainCol = 'PatientEncounterID'
-  p$testWindowCol = 'InTestWindowFLG'
-  p$predictedCol = 'ThirtyDayReadmitFLG'
-  p$impute = TRUE
-  p$debug = FALSE
-  p$useSavedModel = FALSE
-  p$cores = 1
-  p$sqlConn = connection.string
-  p$destSchemaTable = 'dbo.HCRDeployClassificationBASE'
-  p$rfmtry = 3
 
+  csvfile <- system.file("extdata",
+                         "HCRDiabetesClinical.csv",
+                         package = "healthcareai")
+  
+  df <- read.csv(file = csvfile,
+                 header = TRUE,
+                 na.strings =  c('NULL', 'NA', ""))
+  
+  df$PatientID <- NULL
+    
   set.seed(43)
-
-  capture.output(dRF <- RandomForestDeployment$new(p))
-  capture.output(dRF$deploy())
-
-  expect_true(as.numeric(dRF$getPredictedValsForUnitTest()) - 0.0377 < 1.0e-4)
+  p <- SupervisedModelDevelopmentParams$new()
+  p$df <- df
+  p$type <- "classification"
+  p$impute <- TRUE
+  p$grainCol <- "PatientEncounterID"
+  p$predictedCol <- "ThirtyDayReadmitFLG"
+  p$debug <- FALSE
+  p$cores <- 1
+  
+  dRF <- RandomForestDevelopment$new(p)
+  capture.output(dRF$run())
+  
+  # Read from saved model and create predictions
+  p2 <- SupervisedModelDeploymentParams$new()
+  p2$type <- "classification"
+  p2$df <- df
+  p2$testWindowCol <- "InTestWindowFLG"
+  p2$grainCol <- "PatientEncounterID"
+  p2$predictedCol <- "ThirtyDayReadmitFLG"
+  # TODO: remove saved model flag. 
+  p2$useSavedModel <- TRUE # this is always TRUE now.
+  p2$impute <- TRUE
+  p2$writeToDB <- FALSE
+  
+  capture.output(dRF <- RandomForestDeployment$new(p2))
+  suppressWarnings(capture.output(dRF$deploy()))
+  capture.output(dfRes <- dRF$getOutDf())
+  
+  expect_true(as.numeric(dfRes$PredictedProbNBR[1]) - 0.03285276 < 1.0e-2)
 })
 
-test_that("lasso predicted val (with spec mtry) is the same each time", {
+test_that("rf predicted val (w/ mtry tuning) is the same each time", {
   skip_on_travis()
   skip_on_cran()
-  p <- SupervisedModelDeploymentParams$new()
-  p$type = 'classification'
-  p$df = df
-  p$grainCol = 'PatientEncounterID'
-  p$testWindowCol = 'InTestWindowFLG'
-  p$predictedCol = 'ThirtyDayReadmitFLG'
-  p$impute = TRUE
-  p$debug = FALSE
-  p$useSavedModel = FALSE
-  p$cores = 1
-  p$sqlConn = connection.string
-  p$destSchemaTable = 'dbo.HCRDeployClassificationBASE'
-  p$rfmtry = 3
-
+  
+  csvfile <- system.file("extdata",
+                         "HCRDiabetesClinical.csv",
+                         package = "healthcareai")
+  
+  df <- read.csv(file = csvfile,
+                 header = TRUE,
+                 na.strings =  c('NULL', 'NA', ""))
+  
+  df$PatientID <- NULL
+  
   set.seed(43)
-
-  capture.output(dL <- LassoDeployment$new(p))
-  capture.output(dL$deploy())
-
-  expect_equal(as.numeric(dL$getPredictedValsForUnitTest()), 0.02694817)
+  p <- SupervisedModelDevelopmentParams$new()
+  p$df <- df
+  p$type <- "classification"
+  p$grainCol <- "PatientEncounterID"
+  p$predictedCol <- "ThirtyDayReadmitFLG"
+  p$impute <- TRUE
+  p$tune <- TRUE
+  
+  dRF <- RandomForestDevelopment$new(p)
+  capture.output(dRF$run())
+  
+  # Read from saved model and create predictions
+  p2 <- SupervisedModelDeploymentParams$new()
+  p2$type <- "classification"
+  p2$df <- df
+  p2$testWindowCol <- "InTestWindowFLG"
+  p2$grainCol <- "PatientEncounterID"
+  p2$predictedCol <- "ThirtyDayReadmitFLG"
+  # TODO: remove saved model flag. 
+  p2$useSavedModel <- TRUE # this is always TRUE now.
+  p2$impute <- TRUE
+  p2$writeToDB <- FALSE
+  
+  capture.output(dRF <- RandomForestDeployment$new(p2))
+  suppressWarnings(capture.output(dRF$deploy()))
+  capture.output(dfRes <- dRF$getOutDf())
+  expect_true(as.numeric(dfRes$PredictedProbNBR[1]) - 0.04258364 < 1.0e-2)
 })
 
-test_that("rf predicted val (w/out spec mtry) is the same each time", {
+test_that("lasso predicted val is the same each time", {
   skip_on_travis()
   skip_on_cran()
-  p <- SupervisedModelDeploymentParams$new()
-  p$type = 'classification'
-  p$df = df
-  p$grainCol = 'PatientEncounterID'
-  p$testWindowCol = 'InTestWindowFLG'
-  p$predictedCol = 'ThirtyDayReadmitFLG'
-  p$impute = TRUE
-  p$debug = FALSE
-  p$useSavedModel = FALSE
-  p$cores = 1
-  p$sqlConn = connection.string
-  p$destSchemaTable = 'dbo.HCRDeployClassificationBASE'
-
+  
+  csvfile <- system.file("extdata",
+                         "HCRDiabetesClinical.csv",
+                         package = "healthcareai")
+  
+  df <- read.csv(file = csvfile,
+                 header = TRUE,
+                 na.strings =  c('NULL', 'NA', ""))
+  
+  df$PatientID <- NULL
+  
   set.seed(43)
-
-  capture.output(dRF <- RandomForestDeployment$new(p))
-  capture.output(dRF$deploy())
-
-  expect_true(as.numeric(dRF$getPredictedValsForUnitTest()) - 0.0347 < 1.0e-4)
+  # Create saved Lasso model
+  p <- SupervisedModelDevelopmentParams$new()
+  p$df <- df
+  p$type <- "classification"
+  p$impute <- TRUE
+  p$grainCol <- "PatientEncounterID"
+  p$predictedCol <- "ThirtyDayReadmitFLG"
+  p$debug <- FALSE
+  p$cores <- 1
+  
+  lasso <- LassoDevelopment$new(p)
+  capture.output(lasso$run())
+  
+  # Read from saved model and create predictions
+  p2 <- SupervisedModelDeploymentParams$new()
+  p2$type <- "classification"
+  p2$df <- df
+  p2$testWindowCol <- "InTestWindowFLG"
+  p2$grainCol <- "PatientEncounterID"
+  p2$predictedCol <- "ThirtyDayReadmitFLG"
+  # TODO: remove saved model flag. 
+  p2$useSavedModel <- TRUE # this is always TRUE now.
+  p2$impute <- TRUE
+  p2$debug <- TRUE
+  p2$cores <- 1
+  p2$writeToDB <- FALSE
+  
+  capture.output(dL <- LassoDeployment$new(p2))
+  suppressWarnings(capture.output(dL$deploy()))
+  capture.output(dfRes <- dL$getOutDf())
+  
+  expect_true(dfRes$PredictedProbNBR[1] - 0.1052794 < 1.0e-4)
 })
-
-test_that("lasso predicted val (w/out spec mtry) is the same each time", {
-  skip_on_travis()
-  skip_on_cran()
-  p <- SupervisedModelDeploymentParams$new()
-  p$type = 'classification'
-  p$df = df
-  p$grainCol = 'PatientEncounterID'
-  p$testWindowCol = 'InTestWindowFLG'
-  p$predictedCol = 'ThirtyDayReadmitFLG'
-  p$impute = TRUE
-  p$debug = FALSE
-  p$useSavedModel = FALSE
-  p$cores = 1
-  p$sqlConn = connection.string
-  p$destSchemaTable = 'dbo.HCRDeployClassificationBASE'
-
-  set.seed(43)
-
-  capture.output(dL <- LassoDeployment$new(p))
-  capture.output(dL$deploy())
-
-  expect_equal(as.numeric(dL$getPredictedValsForUnitTest()), 0.02694817)
-})
-
-
-
