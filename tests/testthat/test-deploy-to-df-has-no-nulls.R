2cd008b3b40210d9354b56fd999369b91ff98a70
mmastand <michael.mastanduno@healthcatalyst.com>
2017-05-10 15:58:52 -0600

    All unit tests fixed.

--- tests/testthat/test-deploy-to-df-has-no-nulls.R
+++ tests/testthat/test-deploy-to-df-has-no-nulls.R
@@ -1,33 +1,71 @@
 context("Checking deploy predictions from csv to df")
 
-test_that("Lasso predicted df doesn't have NAs", {
 
   csvfile <- system.file("extdata",
                          "HCRDiabetesClinical.csv",
                          package = "healthcareai")
-  
+
   df <- read.csv(file = csvfile,
                  header = TRUE,
                  na.strings =  c('NULL', 'NA', ""))
-  
-  df$PatientID <- NULL
+
   inTest <- df$InTestWindowFLG # save this for later.
+
+  #### BEGIN TESTS ####
+
+test_that("LMM predicted df doesn't have NAs", {
+
+  df$InTestWindowFLG <- NULL
+  
+  # Create LMM model
+  set.seed(43)
+  p <- initializeParamsForTesting(df)
+  p$type = 'classification'
+  p$personCol = 'PatientID'
+  p$predictedCol = 'ThirtyDayReadmitFLG'
+  
+  LinearMixedModel <- LinearMixedModelDevelopment$new(p)
+  capture.output(suppressWarnings(LinearMixedModel$run()))
+  
+  # Depoy LMM model
+  df$InTestWindowFLG <- inTest
+
+  p2 <- SupervisedModelDeploymentParams$new()
+  p2$type <- "classification"
+  p2$df <- df
+  p2$testWindowCol <- "InTestWindowFLG"
+  p2$grainCol <- "PatientEncounterID"
+  p2$personCol <- "PatientID"
+  p2$predictedCol <- "ThirtyDayReadmitFLG"
+  p2$impute <- TRUE
+  p2$debug <- FALSE
+  p2$cores <- 1
+  p2$writeToDB <- FALSE
+  
+  capture.output(dLMM <- LinearMixedModelDeployment$new(p2))
+  capture.output(suppressWarnings(dLMM$deploy()))
+  capture.output(dfRes <- dLMM$getOutDf())
+  
+  expect_equal(as.numeric(sum(is.na(dfRes))), 0)
+  closeAllConnections()
+})
+test_that("Lasso predicted df doesn't have NAs", {
+
+  df$PatientID <- NULL
   df$InTestWindowFLG <- NULL
   
   # Create lasso model
   set.seed(43)
-  p <- SupervisedModelDevelopmentParams$new()
-  p$df <- df
-  p$type <- "classification"
-  p$impute <- TRUE
-  p$grainCol <- "PatientEncounterID"
-  p$predictedCol <- "ThirtyDayReadmitFLG"
+  p <- initializeParamsForTesting(df)
+  p$type = 'classification'
+  p$predictedCol = 'ThirtyDayReadmitFLG'
   
   lasso <- LassoDevelopment$new(p)
   capture.output(lasso$run())
   
   # Depoy lasso model
   df$InTestWindowFLG <- inTest
+
   p2 <- SupervisedModelDeploymentParams$new()
   p2$type <- "classification"
   p2$df <- df
@@ -48,33 +86,21 @@ test_that("Lasso predicted df doesn't have NAs", {
 })
 
 test_that("rf predicted df doesn't have NAs", {
-  
-  csvfile <- system.file("extdata",
-                         "HCRDiabetesClinical.csv",
-                         package = "healthcareai")
-  
-  df <- read.csv(file = csvfile,
-                 header = TRUE,
-                 na.strings =  c('NULL', 'NA', ""))
-  
-  df$PatientID <- NULL
-  inTest <- df$InTestWindowFLG # save this for later.
+
   df$InTestWindowFLG <- NULL
   
   # Create rf model
   set.seed(43)
-  p <- SupervisedModelDevelopmentParams$new()
-  p$df <- df
-  p$type <- "classification"
-  p$impute <- TRUE
-  p$grainCol <- "PatientEncounterID"
-  p$predictedCol <- "ThirtyDayReadmitFLG"
+  p <- initializeParamsForTesting(df)
+  p$type = 'classification'
+  p$predictedCol = 'ThirtyDayReadmitFLG'
   
   RandomForest <- RandomForestDevelopment$new(p)
   capture.output(RandomForest$run())
   
   # Depoy rf model
   df$InTestWindowFLG <- inTest
+  
   p2 <- SupervisedModelDeploymentParams$new()
   p2$type <- "classification"
   p2$df <- df
@@ -89,54 +115,6 @@ test_that("rf predicted df doesn't have NAs", {
   capture.output(dRF <- RandomForestDeployment$new(p2))
   capture.output(dRF$deploy())
   capture.output(dfRes <- dRF$getOutDf())
-  
-  expect_equal(as.numeric(sum(is.na(dfRes))), 0)
-  closeAllConnections()
-})
-
-test_that("LMM predicted df doesn't have NAs", {
-  
-  csvfile <- system.file("extdata",
-                         "HCRDiabetesClinical.csv",
-                         package = "healthcareai")
-  
-  df <- read.csv(file = csvfile,
-                 header = TRUE,
-                 na.strings =  c('NULL', 'NA', ""))
-  
-  inTest <- df$InTestWindowFLG # save this for later.
-  df$InTestWindowFLG <- NULL
-  
-  # Create LMM model
-  set.seed(43)
-  p <- SupervisedModelDevelopmentParams$new()
-  p$df <- df
-  p$type <- "classification"
-  p$impute <- TRUE
-  p$grainCol <- "PatientEncounterID"
-  p$personCol <- "PatientID"
-  p$predictedCol <- "ThirtyDayReadmitFLG"
-  
-  LinearMixedModel <- LinearMixedModelDevelopment$new(p)
-  capture.output(suppressWarnings(LinearMixedModel$run()))
-  
-  # Depoy LMM model
-  df$InTestWindowFLG <- inTest
-  p2 <- SupervisedModelDeploymentParams$new()
-  p2$type <- "classification"
-  p2$df <- df
-  p2$testWindowCol <- "InTestWindowFLG"
-  p2$grainCol <- "PatientEncounterID"
-  p2$personCol <- "PatientID"
-  p2$predictedCol <- "ThirtyDayReadmitFLG"
-  p2$impute <- TRUE
-  p2$debug <- FALSE
-  p2$cores <- 1
-  p2$writeToDB <- FALSE
-  
-  capture.output(dLMM <- LinearMixedModelDeployment$new(p2))
-  capture.output(suppressWarnings(dLMM$deploy()))
-  capture.output(dfRes <- dLMM$getOutDf())
   
   expect_equal(as.numeric(sum(is.na(dfRes))), 0)
   closeAllConnections()
