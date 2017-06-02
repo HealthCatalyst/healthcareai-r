a9176327b0f20326a190b637e7790a6336b13ba7
Mike Mastanduno <mike.mastanduno@gmail.com>
2017-06-02 12:14:46 -0700

    fixed a bug causing outDF to be factors

--- R/xgboost-deployment.R
+++ R/xgboost-deployment.R
@@ -180,9 +180,9 @@
         function(i) colnames(private$temp_predictions)[maxColInds[i,]]))
 
       # combine top 3 maxColVals and maxColNames to make maxProbDF
-      private$orderedProbs <- cbind(maxColVals[,1], maxColNames[,1],
-         maxColVals[,2], maxColNames[,2],
-         maxColVals[,3], maxColNames[,3])
+      private$orderedProbs <- data.frame(as.numeric(maxColVals[,1]), maxColNames[,1],
+         as.numeric(maxColVals[,2]), maxColNames[,2],
+         as.numeric(maxColVals[,3]), maxColNames[,3])
 
       # update column names
       colnames(private$orderedProbs) <- c('PredictedProb1','PredictedClass1',
