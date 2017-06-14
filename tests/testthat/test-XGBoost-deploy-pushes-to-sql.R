3c58cbcf8d43ae7f66bb1ce07b59d75157ea54ef
Hanlei-Zhu <zhuhanlei95@gmail.com>
2017-06-14 17:02:44 -0600

    Write XGBoost to SQL (issues 455-458)

---
+++ tests/testthat/test-XGBoost-deploy-pushes-to-sql.R
@@ -0,0 +1,87 @@
+context("Checking XGBoost deploy predictions from sql server to sql server")
+
+connection.string <- "
+driver={SQL Server};
+server=localhost;
+database=SAM;
+trusted_connection=true
+"
+
+sqliteFile <- system.file("extdata",
+                          "unit-test.sqlite",
+                          package = "healthcareai")
+
+csvfile <- system.file("extdata", 
+                       "dermatology_multiclass_data.csv", 
+                      package = "healthcareai")
+
+df <- read.csv(file = csvfile, header = TRUE, 
+             stringsAsFactors = FALSE,
+             na.strings = c("NULL", "NA", "", "?"))
+
+dfDeploy <- df[347:366,] 
+
+set.seed(43)
+p <- SupervisedModelDevelopmentParams$new()
+p$df <- df
+p$type <- "multiclass"
+p$impute <- TRUE
+p$grainCol <- "PatientID"
+p$predictedCol <- "target"
+p$debug <- FALSE
+p$cores <- 1
+
+p$xgb_params <- list("objective" = "multi:softprob",
+                     "eval_metric" = "mlogloss",
+                     "max_depth" = 6, 
+                     "eta" = 0.1, 
+                     "silent" = 0, 
+                     "nthread" = 2) 
+
+#### BEGIN TESTS ####
+
+test_that("XGBoost deploy pushes values to SQL Server", {
+
+  boost <- XGBoostDevelopment$new(p)
+  capture.output(boost$run())
+  
+  p2 <- SupervisedModelDeploymentParams$new()
+  p2$type <- "multiclass"
+  p2$df <- dfDeploy
+  p2$grainCol <- "PatientID"
+  p2$predictedCol <- "target"
+  p2$impute <- TRUE
+  p2$debug <- FALSE
+  
+  capture.output(boostD <- XGBoostDeployment$new(p2))
+  capture.output(boostD$deploy())
+  capture.output(outDf <- boostD$getOutDf())
+  expect_output(writeData(MSSQLConnectionString = connection.string,
+                          df = outDf,
+                          tableName = 'dermatologyDeployClassificationBASE'),
+                "20 rows were inserted into the SQL Server table dermatologyDeployClassificationBASE")
+})
+
+
+test_that("XGBoost deploy pushes values to SQLite", {
+  
+  boost <- XGBoostDevelopment$new(p)
+  capture.output(suppressWarnings(boost$run()))
+  
+  p2 <- SupervisedModelDeploymentParams$new()
+  p2$type <- "multiclass"
+  p2$df <- dfDeploy
+  p2$grainCol <- "PatientID"
+  p2$predictedCol <- "target"
+  p2$impute <- TRUE
+  p2$debug <- FALSE
+  
+  capture.output(boostD <- XGBoostDeployment$new(p2))
+  capture.output(boostD$deploy())
+  capture.output(outDf <- boostD$getOutDf())
+  
+  expect_output(writeData(SQLiteFileName = sqliteFile,
+                          df = outDf,
+                          tableName = 'dermatologyDeployMulticlassBASE'),
+                "20 rows were inserted into the SQLite table dermatologyDeployMulticlassBASE")
+})
