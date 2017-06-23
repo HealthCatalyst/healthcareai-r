5a09e1707ea10b262f3a08a049ccc0ccafacec95
yvanhuele <yannick.vanhuele@healthcatalyst.com>
2017-06-23 16:05:45 -0600

    Revert "sapply and lapply for different cases"
    This reverts commit d3ec0d7ea02a8d91d4001afafd808e8c797904fe.

--- R/supervised-model-development.R
+++ R/supervised-model-development.R
@@ -187,13 +187,8 @@ SupervisedModelDevelopment <- R6Class("SupervisedModelDevelopment",
       
       # Remove factors levels which don't actually occur in the training data
       factors <- sapply(self$params$df, is.factor)
-      if (is.data.frame(self$params$df[, factors])){
-        self$params$df[, factors] <- lapply(self$params$df[, factors], as.character)
-        self$params$df[, factors] <- lapply(self$params$df[, factors], as.factor)
-      } else {
-        self$params$df[, factors] <- sapply(self$params$df[, factors], as.character)
-        self$params$df[, factors] <- sapply(self$params$df[, factors], as.factor)
-      }
+      self$params$df[, factors] <- sapply(self$params$df[, factors], as.character)
+      self$params$df[, factors] <- sapply(self$params$df[, factors], as.factor)
 
       if (isTRUE(self$params$debug)) {
         print('Entire data set after converting to df and chr to factor')
