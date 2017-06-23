d193be87789402607185ac0b2cedb868a8afdad8
yvanhuele <yannick.vanhuele@healthcatalyst.com>
2017-06-23 16:11:53 -0600

    sapply and lapply for different cases
    It seems sapply is needed for single columns and lapply is needed for
    multiple columns

--- R/supervised-model-development.R
+++ R/supervised-model-development.R
@@ -186,9 +186,15 @@ SupervisedModelDevelopment <- R6Class("SupervisedModelDevelopment",
       self$params$df <- as.data.frame(unclass(self$params$df))
       
       # Remove factors levels which don't actually occur in the training data
+      # Different case for single column vs. multiple columns
       factors <- sapply(self$params$df, is.factor)
-      self$params$df[, factors] <- lapply(self$params$df[, factors], as.character)
-      self$params$df[, factors] <- lapply(self$params$df[, factors], as.factor)
+      if (is.data.frame(self$params$df[, factors])) { # multiple columns
+        self$params$df[, factors] <- lapply(self$params$df[, factors], as.character)
+        self$params$df[, factors] <- lapply(self$params$df[, factors], as.factor)
+      } else {# single column
+        self$params$df[, factors] <- sapply(self$params$df[, factors], as.character)
+        self$params$df[, factors] <- sapply(self$params$df[, factors], as.factor)
+      }
 
       if (isTRUE(self$params$debug)) {
         print('Entire data set after converting to df and chr to factor')
