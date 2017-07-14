3ee1aff1d406e4e194e58a1cc881154cface70e8
Ethan Taft <ethan.taft@healthcatalyst.com>
2017-07-14 13:45:40 -0600

    Created a new common file for error checking
    Added a new function to packaged called isnotYNOrNA  for testing whether
    or not predictedCol is Y/N

---
+++ R/common-error-checking-utils.R
@@ -0,0 +1,24 @@
+#' @title 
+#' Boolean. Tests whether predictedCol is Y/N
+#' 
+#' @description Returns a logical, TRUE or FALSE, depending on what is
+#' contained in the df or vector
+#' @param x A data frame column, vector, or matrix column
+#' @return TRUE or FALSE depending on test
+#'
+#' @export
+#' @references \url{http://healthcare.ai}
+#' @seealso \code{\link{healthcareai}}
+#' @examples
+#' dat <- data.frame(a = c(0,1,1,0,0,0,1,1,1,0,1,0,1,0,1,0,1,1,1,0))
+#' dat2 <- data.frame(a = c(3, 4, 5, 6, 7, 8, 9))
+#' dat3 <- data.frame(a = c('Y', 'N', 'Y', 'N'))
+#' isnotYN(dat[, 1])
+#' isnotYN(dat2[, 1]) 
+#' isnotYN(dat3[, 1])              
+#'                 
+isnotYNOrNA <- function(x){
+  '%nin%' = Negate('%in%')
+  boolResult <- any(x %nin% c('Y', 'N', NA))
+  boolResult
+}
