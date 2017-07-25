8f84bb7b796fc0df98ecf642c8f246ac3cf0ec12
Ethan Taft <ethan.taft@healthcatalyst.com>
2017-07-25 16:24:11 -0600

    Added regression deploy specific values tests
    Added tests for regression algos in deploy specific values.  Updated
    some test names to differentiate between which tests were classification
    and regression.

--- tests/testthat/test-deploy-specific-values.R
+++ tests/testthat/test-deploy-specific-values.R
@@ -12,7 +12,7 @@ context("Checking deploy supervised model prediction values to DF")
 
   #### BEGIN TESTS ####
 
-test_that("mixed model predicted val is the same each time", {
+test_that("mixed model classification predicted val is the same each time", {
   
   # Create LMM model
   set.seed(43)
@@ -44,8 +44,42 @@ test_that("mixed model predicted val is the same each time", {
   expect_true(abs(dfRes$PredictedProbNBR[10] - 0.9881623) < 1.0e-2)
   closeAllConnections()
 })
+  
+test_that("mixed model regression predicted val is the same each time", {
+    
+  # Create LMM model
+  set.seed(43)
+  p <- initializeParamsForTesting(df)
+  p$type = 'regression'
+  p$personCol = 'PatientID'
+  p$predictedCol = 'SystolicBPNBR'
+    
+  LinearMixedModel <- LinearMixedModelDevelopment$new(p)
+  capture.output(suppressWarnings(LinearMixedModel$run()))
+    
+  # Depoy LMM model
+    
+  p2 <- SupervisedModelDeploymentParams$new()
+  p2$type <- "regression"
+  p2$df <- dfDeploy
+  p2$grainCol <- "PatientEncounterID"
+  p2$personCol <- "PatientID"
+  p2$predictedCol <- "SystolicBPNBR"
+  p2$impute <- TRUE
+  p2$debug <- FALSE
+  p2$cores <- 1
+    
+  capture.output(dLMM <- LinearMixedModelDeployment$new(p2))
+  capture.output(suppressWarnings(dLMM$deploy()))
+  capture.output(dfRes <- dLMM$getOutDf())
+    
+  expect_equal(dfRes$PredictedValueNBR[1], 128.0015, tolerance = 1.249115e-05)
+  expect_equal(dfRes$PredictedValueNBR[10], 163.5445, tolerance = 2.728878e-05)
+  closeAllConnections()
+})
 
-test_that("rf predicted val (w/out mtry tuning) is the same each time", {
+test_that("rf classification predicted val (w/out mtry tuning) is the same each 
+          time", {
 
   df$PatientID <- NULL # affects all future tests
   dfDeploy <- df[951:1000,]
@@ -79,7 +113,11 @@ test_that("rf predicted val (w/out mtry tuning) is the same each time", {
   closeAllConnections()
 })
 
-test_that("rf predicted val (w/ mtry tuning) is the same each time", {
+test_that("rf classification predicted val (w/ mtry tuning) is the same each 
+          time", {
+
+  df$PatientID <- NULL # affects all future tests
+  dfDeploy <- df[951:1000,]
   
   # Create rf model
   set.seed(43)
@@ -112,7 +150,82 @@ test_that("rf predicted val (w/ mtry tuning) is the same each time", {
   closeAllConnections()
 })
 
-test_that("lasso predicted val is the same each time", {
+test_that("rf regression predicted val (w/out mtry tuning) is same each time", {
+  
+  df$PatientID <- NULL
+  dfDeploy <- df[951:1000,]
+  
+  # Create rf model
+  set.seed(43)
+  p <- initializeParamsForTesting(df)
+  p$type = 'regression'
+  p$predictedCol = 'SystolicBPNBR'
+  
+  capture.output(RandomForest <- RandomForestDevelopment$new(p))
+  capture.output(RandomForest$run())
+  
+  # Depoy rf model
+  
+  p2 <- SupervisedModelDeploymentParams$new()
+  p2$type <- "regression"
+  p2$df <- dfDeploy
+  p2$grainCol <- "PatientEncounterID"
+  p2$predictedCol <- "SystolicBPNBR"
+  p2$impute <- TRUE
+  p2$debug <- FALSE
+  p2$cores <- 1
+  
+  capture.output(dRF <- RandomForestDeployment$new(p2))
+  capture.output(dRF$deploy())
+  capture.output(dfRes <- dRF$getOutDf())
+  
+  #here, tolerances need to be high in order for the Travis build to work on mac
+  expect_equal(dfRes$PredictedValueNBR[7], 157.333, tolerance = 2.300645e-05)
+  expect_equal(dfRes$PredictedValueNBR[19], 148.4243, tolerance = 5.779318e-06)
+  closeAllConnections()
+})
+
+
+test_that("rf regression predicted val (w/mtry tuning) is same each time", {
+  
+  df$PatientID <- NULL
+  dfDeploy <- df[951:1000,]
+  
+  # Create rf model
+  set.seed(43)
+  p <- initializeParamsForTesting(df)
+  p$type = 'regression'
+  p$predictedCol = 'SystolicBPNBR'
+  p$tune <- TRUE
+  
+  capture.output(RandomForest <- RandomForestDevelopment$new(p))
+  capture.output(RandomForest$run())
+  
+  # Depoy rf model
+  
+  p2 <- SupervisedModelDeploymentParams$new()
+  p2$type <- "regression"
+  p2$df <- dfDeploy
+  p2$grainCol <- "PatientEncounterID"
+  p2$predictedCol <- 'SystolicBPNBR'
+  p2$impute <- TRUE
+  p2$debug <- FALSE
+  p2$cores <- 1
+  
+  capture.output(dRF <- RandomForestDeployment$new(p2))
+  capture.output(dRF$deploy())
+  capture.output(dfRes <- dRF$getOutDf())
+  
+  # for some reason, this tolerance needs to be very lax...
+  expect_equal(dfRes$PredictedValueNBR[15], 136.2595, tolerance = 4.104058e-05)
+  expect_equal(dfRes$PredictedValueNBR[23], 162.4714, tolerance = 1.003346e-05)
+  closeAllConnections()
+})
+
+test_that("lasso classification predicted val is the same each time", {
+  
+  df$PatientID <- NULL # affects all future tests
+  dfDeploy <- df[951:1000,]
   
   # Create lasso model
   set.seed(43)
@@ -140,5 +253,39 @@ test_that("lasso predicted val is the same each time", {
   
   expect_true(abs(dfRes$PredictedProbNBR[1] - 0.1566765) < 1.0e-2)
   expect_true(abs(dfRes$PredictedProbNBR[10] - 0.2221281) < 1.0e-2)
+  closeAllConnections()
+})
+
+test_that("lasso regression predicted val is the same each time", {
+  
+  df$PatientID <- NULL
+  dfDeploy <- df[951:1000,]
+  
+  # Create lasso model
+  set.seed(43)
+  p <- initializeParamsForTesting(df)
+  p$type = 'regression'
+  p$predictedCol = 'SystolicBPNBR'
+  
+  lasso <- LassoDevelopment$new(p)
+  capture.output(lasso$run())
+  
+  # Depoy lasso model
+  
+  p2 <- SupervisedModelDeploymentParams$new()
+  p2$type <- "regression"
+  p2$df <- dfDeploy
+  p2$grainCol <- "PatientEncounterID"
+  p2$predictedCol <- "SystolicBPNBR"
+  p2$impute <- TRUE
+  p2$debug <- FALSE
+  p2$cores <- 1
+  
+  capture.output(dL <- LassoDeployment$new(p2))
+  capture.output(dL$deploy())
+  capture.output(dfRes <- dL$getOutDf())
+  
+  expect_equal(dfRes$PredictedValueNBR[50], 149.6703, tolerance = 2.460667e-06)
+  expect_equal(dfRes$PredictedValueNBR[27], 149.6703, tolerance = 2.460667e-06)
   closeAllConnections()
 })
