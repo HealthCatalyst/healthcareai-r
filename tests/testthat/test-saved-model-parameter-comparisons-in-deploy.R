d0fec406fda862d3a827a626a798fe17b8ac0fe4
yvanhuele <yannick.vanhuele@healthcatalyst.com>
2017-08-25 15:24:53 -0600

    added some unit tests

---
+++ tests/testthat/test-saved-model-parameter-comparisons-in-deploy.R
@@ -0,0 +1,115 @@
+# These tests verify that certain parameters of a saved model such as the model
+# type (classification, etc.) are compared to the deployment parameters
+# before deployment
+
+context("Checking that saved models are vetted in deployment")
+
+test_that("Classification models can only be used for classification", {
+  # Build a toy dataframe for classification
+  n <- 100
+  d <- data.frame(id = 1:n,
+                  x = rnorm(n),
+                  y = rnorm(n),
+                  z = rnorm(n))
+  d$response <- ifelse(d$x + d$y + d$z  + rnorm(n) > 0, "Y", "N")
+  
+  # Set up development parameters
+  p <- SupervisedModelDevelopmentParams$new()
+  p$df <- d
+  p$type <- "classification"
+  p$predictedCol <- "response"
+  p$grainCol <- "id"
+  
+  # Develop a classification model
+  capture.output(rf <- RandomForestDevelopment$new(p))
+  capture.output(rf$run())
+  
+  # Set up deployment parameters
+  p2 <- SupervisedModelDeploymentParams$new()
+  p2$df <- d
+  p2$type <- "regression" # param is incorrectly set to regression
+  p2$predictedCol <- "response"
+  p2$grainCol <- "id"
+  
+  expect_error(capture.output(rfD <- RandomForestDeployment$new(p2)),
+               paste0("The saved model you have loaded is a classification ",
+                      "model, but you are trying to deploy a regression model"))
+  
+  p2$type <- "multiclass" # param is incorrectly set to multiclass
+  
+  expect_error(capture.output(rfD <- RandomForestDeployment$new(p2)),
+               paste0("The saved model you have loaded is a classification ",
+                      "model, but you are trying to deploy a multiclass model"))
+})
+
+test_that("Regression models can only be used for regression", {
+  # Build a toy dataframe for regression
+  n <- 100
+  d <- data.frame(id = 1:n,
+                  x = rnorm(n),
+                  y = rnorm(n),
+                  z = rnorm(n))
+  d$response <- d$x + d$y + d$z  + rnorm(n)
+  
+  # Set up development parameters
+  p <- SupervisedModelDevelopmentParams$new()
+  p$df <- d
+  p$type <- "regression"
+  p$predictedCol <- "response"
+  p$grainCol <- "id"
+  
+  # Develop a regression model
+  capture.output(rf <- RandomForestDevelopment$new(p))
+  capture.output(rf$run())
+  
+  # Set up deployment parameters
+  p2 <- SupervisedModelDeploymentParams$new()
+  p2$df <- d
+  p2$type <- "classification" # param is incorrectly set to classification
+  p2$predictedCol <- "response"
+  p2$grainCol <- "id"
+  
+  expect_error(capture.output(rfD <- RandomForestDeployment$new(p2)),
+               paste0("The saved model you have loaded is a regression ",
+                      "model, but you are trying to deploy a classification ",
+                      "model"))
+  
+  p2$type <- "multiclass" # param is incorrectly set to multiclass
+  
+  expect_error(capture.output(rfD <- RandomForestDeployment$new(p2)),
+               paste0("The saved model you have loaded is a regression ",
+                      "model, but you are trying to deploy a multiclass model"))
+})
+
+test_that("Warning is thrown if predicted columns don't match", {
+  # Build toy dataframe with "response" as the predicted column
+  n <- 100
+  d <- data.frame(id = 1:n,
+                  x = rnorm(n),
+                  y = rnorm(n),
+                  z = rnorm(n))
+  d$response <- ifelse(d$x + d$y + d$z  + rnorm(n) > 0, "Y", "N")
+  
+  # Set up development parameters and develop model
+  p <- SupervisedModelDevelopmentParams$new()
+  p$df <- d
+  p$type <- "classification"
+  p$predictedCol <- "response" # lowercase predicted column
+  p$grainCol <- "id"
+  capture.output(rf <- RandomForestDevelopment$new(p))
+  capture.output(rf$run())
+  
+  # Set up deployment parameters
+  p2 <- SupervisedModelDeploymentParams$new()
+  p2$df <- d
+  p2$type <- "classification"
+  p2$predictedCol <- "RESPONSE" # misspelled predicted column
+  p2$grainCol <- "id"
+  
+  expect_warning(capture.output(rfD <- RandomForestDeployment$new(p2)), 
+                 paste0("The name of the predicted column in the saved model ",
+                        "differs from the name of the predicted column that ", 
+                        "you have set.", 
+                        "\n- Old predicted column: response", 
+                        "\n- New predicted column: RESPONSE"))
+})
