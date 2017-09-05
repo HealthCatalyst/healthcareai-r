eabdda9b38640e010ab0bf8c79a1f48d5d065b92
Mike Mastanduno <michael.mastanduno@healthcatalyst.com>
2017-09-05 14:00:24 -0600

    Fixed up risk adjusted comparisons

--- tests/testthat/test-risk-adjusted-comparisons.R
+++ tests/testthat/test-risk-adjusted-comparisons.R
@@ -22,12 +22,8 @@ set.seed(5)
 riskAdjComp <- RiskAdjustedComparisons$new(p)
 capture.output(riskAdjComp$run())
 
-# test_that("Risk-adjusted comparison is as expected for group 1", {
-#   print(riskAdjComp$dfReturn[1,'comparativePerformance']) # for debugging.
-#   expect_identical(riskAdjComp$dfReturn[1,'comparativePerformance'], -5.5)
-# })
-# 
-# test_that("Risk-adjusted comparison is as expected for group 2", {
-#   print(riskAdjComp$dfReturn[2,'comparativePerformance']) # for debugging.
-#   expect_identical(riskAdjComp$dfReturn[2,'comparativePerformance'], 5.5)
-# })
+dfExp <- data.frame(groupbyList=c('M', 'F'), comparativePerformance=c(-4,4))
+
+test_that("Risk-adjusted comparison runs", {
+  expect_equal(riskAdjComp$dfReturn, dfExp)
+})
