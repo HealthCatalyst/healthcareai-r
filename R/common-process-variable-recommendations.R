de48b261e4f2023ef537477bef04001a3475f6ae
yvanhuele <yannick.vanhuele@healthcatalyst.com>
2017-09-18 10:37:24 -0600

    cleaned up code

--- R/common-process-variable-recommendations.R
+++ R/common-process-variable-recommendations.R
@@ -1,9 +1,50 @@
-build_process_df <- function(dataframe,
-                             modifiable_variable_levels) {
+library(tidyverse)
+
+build_process_variable_df_list <- function(dataframe,
+                                           modifiable_variable_levels,
+                                           predict_function, 
+                                           low_probabilities_desired) {
+  # Add row numbers to 
+  dataframe["df_row_number"] <- 1:nrow(dataframe)
+  # Build big dataframe of permuted data
+  permuted_df <- permute_process_variables(dataframe,
+                                           modifiable_variable_levels)
+  
+  tracking_columns <- c("df_row_number", "process_variable_name", "alt_value")
+  
+  # Make Predictions 
+  dataframe$base_prediction <- predict_function(newData = dataframe[!names(dataframe) %in% tracking_columns])
+  permuted_df$new_prediction <- predict_function(newData = permuted_df[!names(permuted_df) %in% tracking_columns])
+
+    
+  full_df <- dataframe %>%
+    # Join on row number
+    dplyr::inner_join(permuted_df, by = "df_row_number") %>%
+    # Add delta column
+    dplyr::mutate(delta = new_prediction - base_prediction) %>% 
+    # Restrict to desired columns
+    dplyr::select(df_row_number,
+                  process_variable_name, 
+                  current_value, 
+                  alt_value, 
+                  base_prediction, 
+                  new_prediction, 
+                  delta)
+  
+  ordering_direction <- (-1)^low_probabilities_desired
+  
+  lapply(1:nrow(dataframe), function(i) {
+    full_df %>%
+      dplyr::filter(df_row_number == i) %>%
+      dplyr::arrange(ordering_direction*desc(delta)) %>%
+      dplyr::select(-c(df_row_number))
+  })
+}
+
+permute_process_variables <- function(dataframe,
+                                      modifiable_variable_levels) {
   modifiable_names <- names(modifiable_variable_levels)
 
-  # Split up by variable
-  
   # Copy for each level
   do.call(rbind, lapply(seq_along(modifiable_variable_levels), function(i) {
     # Fix variable
@@ -15,24 +56,25 @@ build_process_df <- function(dataframe,
       # Replace modifiable variable with level
       level <- levels[[j]]
       df[[modifiable_variable]] <- level
+      df["current_value"] <- as.character(dataframe[[modifiable_variable]])
       df["alt_value"] <- as.character(level)
       # Return the modified dataframe
       df
     }))
     one_variable_df["process_variable_name"] <- as.character(modifiable_variable)
+    # 
     one_variable_df
   }))
 }
 
 
-
-buildProcessVariablesDf = function(modFactorsList,
-                                   repeatedFactors = FALSE,
-                                   numTopFactors = 3) {
+build_process_variables_df = function(modFactorsList,
+                                      repeatedFactors = FALSE,
+                                      numTopFactors = 3) {
   # Drop repeated rows, if desired
   if (!repeatedFactors) {
-    modFactorsList <- dropRepeated(dfList = modFactorsList, 
-                                   columnName = "variable")
+    modFactorsList <- drop_repeated(df_list = modFactorsList, 
+                                    column_name = "process_variable_name")
   }
   
   # Aetermine the maximum number of modifiable factors
@@ -40,7 +82,7 @@ buildProcessVariablesDf = function(modFactorsList,
   # Combine into dataframe
   modFactorsDf <- do.call(rbind, lapply(modFactorsList, function(rowDf){
     do.call(cbind, lapply(1:numTopFactors, function(i) {
-      row <- rowDf[i, ]
+      row <- rowDf[i, !(names(rowDf) %in% c("base_prediction"))]
       names(row) <- c(paste0("Modify", i, "TXT"),
                       paste0("Modify", i, "Current"), 
                       paste0("Modify", i, "AltValue"),
