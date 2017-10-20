ea17fbdee86cbb7ca348010c8d9256b4317b3ee7
josephtrahan <32748961+josephtrahan@users.noreply.github.com>
2017-10-20 09:20:31 -0400

    added plot_flg, changed some formatting, output violations and plot, includes example code to be merged into documentation

--- R/common-nelson-rules.R
+++ R/common-nelson-rules.R
@@ -4,7 +4,10 @@
 #' Search within a data frame to find violations of Nelson Rule 1.
 #' Nelson Rule 1: One point is more than 3 standard deviations from the mean.
 #' @param df A data frame
-#' @param dateCol A string denoting the date column
+#' @param measureCol A string denoting the column that contains the numeric
+#' value to be evaluated by the nelsonRule1 function
+#' @param dateCol A string denoting the date column used for evaluating
+#' nelsonRule1 over time
 #' @return A data frame containing the date, measure value, uppper control
 #' limit, lower control limit, a flag indicating whether or not a rule violation
 #' occured and a description of the violation.
@@ -33,29 +36,68 @@
 #'df
 #'
 #'ggplot(data = df
-#'       ,aes(x = date
-#'            ,y = measure)) +
+#'  ,aes(x = date
+#'  ,y = measure)) +
 #'  geom_line() +
 #'  geom_hline(data = unique(df$ucl), yintercept = unique(df$ucl)) +
 #'  geom_hline(data = unique(df$lcl), yintercept = unique(df$lcl)) +
 #'  geom_hline(aes(yintercept = mean(df$measure)), linetype = 'dashed') +
 #'  geom_point(data = df[df$violationFLG == TRUE,]
-#'             ,aes(color = violationFLG))
+#'    ,aes(color = violationFLG))
 
-nelsonRule1 <- function(df, measureCol, dateCol) {
-  ucl <- mean(df[[measureCol]]) + 3 * sd(df[[measureCol]])
-  lcl <- mean(df[[measureCol]]) - 3 * sd(df[[measureCol]])
+nelsonRule1 <- function(df, measure_col, date_col, plot_flg) {
+  library(ggplot2)
   
-  violationFLG <- df[[measureCol]] > ucl | df[[measureCol]] < lcl
+  ucl <- mean(df[[measure_col]]) + 3 * sd(df[[measure_col]])
+  lcl <- mean(df[[measure_col]]) - 3 * sd(df[[measure_col]])
   
-  violationDSC <- ifelse(df[[measureCol]] > ucl, 'more than 3 standard deviations above the mean'
-                         ,ifelse(df[[measureCol]] < lcl, 'more than 3 standard deviations below the mean',NA))
+  violationFLG <- as.numeric(df[[measure_col]] > ucl | df[[measure_col]] < lcl)
   
-  df <- data.frame(date = df[[dateCol]]
-                   ,measure = df[[measureCol]]
-                   ,ucl = ucl
-                   ,lcl = lcl
-                   ,violationFLG = violationFLG
-                   ,violationDSC = violationDSC)
-  df
+  violationDSC <- ifelse(df[[measure_col]] > ucl, 'more than 3 standard deviations above the mean'
+                         ,ifelse(df[[measure_col]] < lcl, 'more than 3 standard deviations below the mean',NA))
+  
+  out <- list()
+  
+  df <- data.frame(date = df[[date_col]]
+         ,measure = df[[measure_col]]
+         ,ucl = ucl
+         ,lcl = lcl
+         ,violationFLG = violationFLG
+         ,violationDSC = violationDSC)
+  
+  colnames(df)[1:2] <- c(date_col,measure_col)
+  
+  ifelse(plot_flg == TRUE,
+    out$p <- ggplot(data = df
+                ,aes(x = date
+                     ,y = measureValue)) +
+          geom_line() +
+          geom_hline(data = unique(df$ucl), yintercept = unique(df$ucl)) +
+          geom_hline(data = unique(df$lcl), yintercept = unique(df$lcl)) +
+          geom_hline(aes(yintercept = mean(measureValue)), linetype = 'dashed') +
+          geom_point(data = df[df$violationFLG == TRUE,]
+                     ,color = 'red'),
+    out$p <- NA
+  )
+  
+  #return(p)
+  
+  out$dfViolations <- df[df$violationFLG == 1,]
+  
+  return(out)
 }
+
+date <- seq.Date(from = as.Date('2016-01-03'), length.out = 52, by = 'week')
+set.seed(34)
+measureValue <- rnorm(length(date), mean = 100, sd = 15)
+
+# Alter some measureValues to be at least 3 standard deviations from the mean
+# so they can be displayed as violation examples.
+measureValue[9] <- 179
+measureValue[19] <- 22
+measureValue[47] <- 177
+
+d <- data.frame(date, measureValue)
+nr1 <- nelsonRule1(df = d, measure_col = 'measureValue', date_col = 'date', plot_flg = TRUE)
+dfViolations <- nr1$dfViolations
+nr1
