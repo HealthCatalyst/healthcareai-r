8c49c417cc74399c406d22ae75423903c47f4a82
Michael Levy <michael.levy@healthcatalyst.com>
2017-10-26 23:43:45 -0600

    data documentation

---
+++ R/data.R
@@ -0,0 +1,9 @@
+#' Patient diabetes dataset
+#'
+#' A dataset containing diabetes status and other health-related variables
+#' for 768 females, at least 21 years old, of Pima Indian heritage.
+#'
+#' @format A tibble data frame with 768 rows and 10 variables:
+#' 
+#' @source \url{https://archive.ics.uci.edu/ml/datasets/pima+indians+diabetes}
+"pima_diabetes"
