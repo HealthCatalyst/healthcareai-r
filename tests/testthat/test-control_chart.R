14b6138f5ea653df7875ca533dd0355e47d02ca1
Michael Levy <michael.levy@healthcatalyst.com>
2017-11-01 15:57:16 -0600

    tests for control_chart

--- tests/testthat/test-control_chart.R
+++ tests/testthat/test-control_chart.R
@@ -1,18 +1,45 @@
 context("Checking control_chart")
 
 # Setup ------------------------------------------------------------------------
+library(ggplot2)
 testD <- tibble::tibble(
   var1 = rep(letters[1:2], each = 10),
   var2 = rep(letters[2:5], times = 5),
   outcome = c(1:19, 25)  # To get different mean and median
 )
 
+# Test control_chart -----------------------------------------------------------
+test_that("control_chart returns a ggplot object", {
+  output <- control_chart(testD, "outcome")
+  expect_true(is.ggplot(output))
+  output <- control_chart(testD, "outcome", group1 = "var1", group2 = "var2")
+  expect_true(is.ggplot(output))
+  output <- control_chart(testD, "outcome", group1 = "var1", group2 = "var2", 
+                          save_to = "tmpFile.png")
+  expect_true(is.ggplot(output))
+})
+
+test_that("control_chart writes file if given a save_to path", {
+  suppressWarnings(invisible(file.remove("tmpFile.png")))
+  output <- control_chart(testD, "outcome", save_to = "tmpFile.png")
+  expect_true(file.exists("tmpFile.png"))
+})
+
+test_that("control_chart takes csv filepath as argumnent", {
+  readr::write_csv(testD, "tmpFile.csv")
+  output <- control_chart(d = "tmpFile.csv", measure = "outcome")
+  expect_true(is.ggplot(output))
+  control_chart(d = "tmpFile.csv", measure = "outcome", save_to = "tmpFile.png")
+})
+
+
+# Test calculate_bounds --------------------------------------------------------
 bounds <- calculate_bounds(d = testD,
                            measure = "outcome",
                            center_line = mean,
                            sigmas = 3)
 
-# Test calculate_bounds --------------------------------------------------------
+
 test_that("calculate_bounds returns a vector of three with correct names", {
   expect_equal(length(bounds), 3L)
   expect_named(bounds)
@@ -36,3 +63,7 @@ test_that("calculate_bounds returns correct values with non-defaults", {
   expect_equal(new_bounds[["mid"]], 10.5, tolerance = .1)
   expect_equal(new_bounds[["lower"]], -2.3, tolerance = .1)
 })
+
+# Clean up ---------------------------------------------------------------------
+file.remove("tmpFile.png")
+file.remove("tmpFile.csv")
