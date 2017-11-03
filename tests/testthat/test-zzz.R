3dce13861a3d3e2dd95e2658ab2ab73e143b5ae7
Michael Levy <michael.levy@healthcatalyst.com>
2017-11-03 11:44:51 -0600

    add teardown from testing to remove files

---
+++ tests/testthat/test-zzz.R
@@ -0,0 +1,9 @@
+context("Teardown")
+
+test_that("Remove files written during testing and confirm they are removed.", {
+    # Remove log files, model data, and plot output
+    to_remove <- list.files(pattern = "(txt$)|(rda$)|(pdf$)|(png$)")
+    file.remove(to_remove)
+    expect_true(length(to_remove) > 0)
+    expect_true(all(!file.exists(to_remove)))
+})
