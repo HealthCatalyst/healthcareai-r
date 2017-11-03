dac52fd9ebc5bc296a9d8c0421f1af06d89eaae5
Michael Levy <michael.levy@healthcatalyst.com>
2017-11-03 15:16:43 -0600

    skip on appveyor

--- tests/testthat/test-zzz.R
+++ tests/testthat/test-zzz.R
@@ -1,6 +1,8 @@
 context("Teardown")
 
 test_that("Remove files written during testing and confirm they are removed.", {
+    skip_on_not_appveyor()
+
     # Remove log files, model data, and plot output
     to_remove <- list.files(pattern = "(txt$)|(rda$)|(pdf$)|(png$)")
     file.remove(to_remove)
