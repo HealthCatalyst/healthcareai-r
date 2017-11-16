bce3bf04f60cee0050a4b9a041948a4f500092a2
Mike Mastanduno <michael.mastanduno@healthcatalyst.com>
2017-11-16 16:03:42 -0700

    defaults working

---
+++ R/hcai-impute.R
@@ -0,0 +1,74 @@
+#' @title
+#' Clean NA values from categorical/nominal variables
+#'
+#' @description `step_hcai_missing` creates a specification of a recipe that 
+#'  will replace NA values `hcai_missing`.
+#' @param recipe A recipe object. The step will be added to the sequence of 
+#'  operations for this recipe.
+#' @param ... One or more selector functions to choose which variables are 
+#'  affected by the step. See [selections()] for more details.
+#' @param role Not used by this step since no new variables are created.
+#' @param trained A logical to indicate if the number of NA values have been
+#'  counted in preprocessing.
+#' @param na_percentage A named numeric vector of NA percentages. This
+#'  is `NULL` until computed by [prep.recipe()].
+#' @return An updated version of `recipe` with the new step
+#'  added to the sequence of existing steps (if any). For the
+#'  `tidy` method, a tibble with columns `terms` (the
+#'  selectors or variables selected) and `value` (the
+#'  NA counts).
+#'
+#' @export
+#' @import recipes
+#' @importFrom rlang quos
+#' @details NA values must g `step_scale` estimates
+#'  the variable standard deviations from the data used in the
+#'  `training` argument of `prep.recipe`.
+#'  `bake.recipe` then applies the scaling to new data sets
+#'  using these standard deviations.
+#' @examples
+#' library(healthcareai)
+#' library(tibble)
+#' library(recipes)
+#' n = 100
+#' d <- tibble(encounter_id = 1:n,
+#'             patient_id = sample(1:20, size = n, replace = TRUE),
+#'             hemoglobin_count = rnorm(n, mean = 15, sd = 1),
+#'             hemoglobin_category = sample(c("Low", "Normal", "High", NA), 
+#'                                          size = n, replace = TRUE),
+#'             disease = ifelse(hemoglobin_count < 15, "Yes", "No")
+#' )
+#' 
+#' # Initialize
+#' my_recipe <- recipe(disease ~ ., data = d)
+#' 
+#' # Create recipe
+#' my_recipe <- my_recipe %>%
+#'   step_hcai_missing(all_nominal())
+#' my_recipe
+#' 
+#' # Train recipe
+#' trained_recipe <- prep(my_recipe, training = d)
+#' 
+#' # Apply recipe
+#' data_modified <- bake(trained_recipe, newdata = d)
+#' 
+hcai_impute <- function(rec_obj, 
+                        numeric_method = "mean",
+                        nominal_method = "new_category") {
+  # Check to make sure rec_obj is the right type
+  if (class(rec_obj) != "recipe") {
+    stop("rec_obj must be recipe object"
+    )
+  }
+  
+  # defaults to mean for numerics.
+  if (numeric_method == "mean") {
+    rec_obj <- step_meanimpute(rec_obj, all_numeric())
+  }
+  
+  # defaults to new category for nominal
+  if (nominal_method == "new_category") {
+    impute_method <- step_hcai_missing(rec_obj, all_nominal())
+  }
+}
---
+++ tests/testthat/test-hcai-impute.R
@@ -0,0 +1,55 @@
+context("Checking recipe step hcai-missing")
+
+library(healthcareai)
+library(tibble)
+library(caret)
+library(recipes)
+
+# Setup ------------------------------------------------------------------------
+# set seed for reproducibility
+set.seed(7)
+# build dataset
+n <- 300
+d <- tibble(id = 1:n,
+                 playerID = sample(1:9, size = n, replace = TRUE),
+                 level = sample(1:12, size = n, replace = TRUE),
+                 world = sample(1:8, size = n, replace = TRUE),
+                 character = sample(c("Mario", "Luigi"), size = n,
+                                    replace = TRUE),
+                 suit = sample(c("Fire Flower", "Raccoon", "Frog", "P Wing"),
+                                    size = n, replace = TRUE)
+                 )
+
+# target
+d["is_goomba"] <- ifelse( (d["world"] - 2 * d["level"] - 1) > 0, "Y", "N")
+
+# Add NAs
+inds <- sample(1:n, 30, replace = FALSE)
+d$suit[inds] <- NA
+inds <- sample(1:n, 100, replace = FALSE)
+d$character[inds] <- NA
+
+train_index <- caret::createDataPartition(
+  d$is_goomba,
+  p = 0.8,
+  times = 1,
+  list = TRUE)
+
+d_train <- d[train_index$Resample1, ]
+d_test <- d[-train_index$Resample1, ]
+
+rec_obj <- recipe(is_goomba ~ ., data = d)
+
+# Tests ------------------------------------------------------------------------
+test_that("Bad rec_obj throws an error", {
+  expect_error(hcai_impute())
+  expect_error(hcai_impute(rec_obj = "yeah hi!"))
+})
+
+test_that("Defaults return mean on numeric, hcai on nominal", {
+  rec_obj_new <- rec_obj %>%
+    hcai_impute()
+
+  expect_equal(class(rec_obj_new$steps[[1]])[1], "step_meanimpute")
+  expect_equal(class(rec_obj_new$steps[[2]])[1], "step_hcai_missing")
+})
