f50a56af531a16546526cc489d32b2cbc2000ddf
Mike Mastanduno <michael.mastanduno@healthcatalyst.com>
2017-12-05 15:30:52 -0700

    Fixed.

--- R/step_hcai_missing.R
+++ R/step_hcai_missing.R
@@ -1,135 +1,153 @@
-context("Checking recipe step hcai-missing")
-
-library(recipes)
-# Setup ------------------------------------------------------------------------
-# set seed for reproducibility
-set.seed(7)
-# build dataset
-n <- 300
-d <- tibble::tibble(id = 1:n,
-                    playerID = sample(1:9, size = n, replace = TRUE),
-                    level = sample(1:12, size = n, replace = TRUE),
-                    world = sample(1:8, size = n, replace = TRUE),
-                    character = sample(c("Mario", "Luigi"), size = n,
-                                       replace = TRUE),
-                    suit = sample(c("Fire Flower", "Raccoon", "Frog", "P Wing"),
-                                  size = n, replace = TRUE)
-)
-
-# target
-d["is_goomba"] <- ifelse( (d["world"] - 2 * d["level"] - 1) > 0, "Y", "N")
-# Add NAs
-inds <- sample(n, 30, replace = FALSE)
-d$suit[inds] <- NA
-inds <- sample(n, 100, replace = FALSE)
-d$character[inds] <- NA
-
-train_index <- caret::createDataPartition(
-  d$is_goomba,
-  p = 0.8,
-  times = 1,
-  list = TRUE)
-
-d_train <- d[train_index$Resample1, ]
-d_test <- d[-train_index$Resample1, ]
-
-rec_obj <- recipe(is_goomba ~ ., data = d)
-
-rec_obj <- rec_obj %>%
-  step_hcai_missing(all_nominal())
-
-junk <- capture_output(
-  rec_obj <- prep(rec_obj, training = d_train)
-)
-
-junk <- capture_output(
-  out_train <- bake(rec_obj, d_train)
-)
-
-junk <- capture_output(
-  out_test <- bake(rec_obj, d_test)
-)
-
-
-d$koopa = sample(c("Blue", "Red", NA), prob = c(.2, .212, .588), 
-                 size = n, replace = TRUE)
-d2_train <- d[train_index$Resample1, ]
-
-rec_obj2 <- recipe(is_goomba ~ ., data = d) %>%
-  step_hcai_missing(starts_with("koop"))
-
-# Tests ------------------------------------------------------------------------
-test_that("Recipe object is updated with step", {
-  expect_equal(class(rec_obj$steps[[1]])[1], "step_hcai_missing")
-})
-
-test_that("Recipe is prepped correctly", {
-  expect_equal(
-    rec_obj$steps[[1]]$na_percentage[[1]],
-    33.195, tolerance = .001)
-  
-  expect_equal(
-    names(rec_obj$steps[[1]]$na_percentage)[1],
-    "character")
-  
-  expect_equal(
-    rec_obj$steps[[1]]$na_percentage[[2]],
-    8.714, tolerance = .001)
-  
-  expect_equal(
-    names(rec_obj$steps[[1]]$na_percentage)[2],
-    "suit")
-})
-
-test_that("Recipe is baked correctly on training data", {
-  expect_true("hcai_missing" %in% levels(out_train$character))
-  
-  expect_equal(
-    sum(out_train$character == "hcai_missing"),
-    80)
-  
-  expect_true("hcai_missing" %in% levels(out_train$suit))
-  
-  expect_equal(
-    sum(out_train$suit == "hcai_missing"),
-    21)
-})
-
-test_that("Recipe is baked correctly on test data", {
-  expect_true("hcai_missing" %in% levels(out_test$character))
-  
-  expect_equal(
-    sum(out_test$character == "hcai_missing"),
-    20)
-  
-  expect_true("hcai_missing" %in% levels(out_test$suit))
-  
-  expect_equal(
-    sum(out_test$suit == "hcai_missing"),
-    9)
-})
-
-test_that("Printer method works correctly within print.recipe()", {
-  expect_output(
-    print(rec_obj),
-    regexp = "[Filling NA with hcai_missing for character, suit]"
+#' @title
+#' Clean NA values from categorical/nominal variables
+#'
+#' @description \code{step_hcai_missing} creates a specification of a recipe that
+#'  will replace NA values with a new factor level, \code{hcai_missing}.
+#' @param recipe A recipe object. The step will be added to the sequence of
+#'  operations for this recipe.
+#' @param ... One or more selector functions to choose which variables are
+#'  affected by the step. See \code{?recipes::selections()} for more details.
+#' @param role Not used by this step since no new variables are created.
+#' @param trained A logical to indicate if the number of NA values have been
+#'  counted in preprocessing.
+#' @param na_percentage A named numeric vector of NA percentages. This
+#'  is \code{NULL} until computed by \code{prep.recipe()}.
+#' @return An updated version of \code{recipe} with the new step
+#'  added to the sequence of existing steps (if any). For the
+#'  \code{tidy} method, a tibble with columns \code{terms} (the
+#'  selectors or variables selected) and \code{value} (the
+#'  NA counts).
+#'
+#' @export
+#' @import recipes
+#' @importFrom rlang quos
+#' @details NA values are counted when the recipe is trained using
+#' \code{prep.recipe}. \code{bake.recipe} then fills in the missing values for
+#' the new data.
+#' @examples
+#' library(healthcareai)
+#' library(tibble)
+#' library(recipes)
+#' n = 100
+#' d <- tibble(encounter_id = 1:n,
+#'             patient_id = sample(1:20, size = n, replace = TRUE),
+#'             hemoglobin_count = rnorm(n, mean = 15, sd = 1),
+#'             hemoglobin_category = sample(c("Low", "Normal", "High", NA),
+#'                                          size = n, replace = TRUE),
+#'             disease = ifelse(hemoglobin_count < 15, "Yes", "No")
+#' )
+#'
+#' # Initialize
+#' my_recipe <- recipe(disease ~ ., data = d)
+#'
+#' # Create recipe
+#' my_recipe <- my_recipe %>%
+#'   step_hcai_missing(all_nominal())
+#' my_recipe
+#'
+#' # Train recipe
+#' trained_recipe <- prep(my_recipe, training = d)
+#'
+#' # Apply recipe
+#' data_modified <- bake(trained_recipe, newdata = d)
+#'
+step_hcai_missing <- function(recipe,
+                              ...,
+                              role = NA,
+                              trained = FALSE,
+                              na_percentage = NULL) {
+  terms <- quos(...)
+  if (length(terms) == 0)
+    stop("Please supply at least one variable specification. See ?selections.")
+  add_step(
+    recipe,
+    step_hcai_missing_new(
+      terms = terms,
+      role = role,
+      trained = trained,
+      na_percentage = na_percentage
+    )
   )
-})
+}
 
-test_that("Warning is triggered for greater than 50% NA", {
-  expect_message(
-    junk <- capture_output(
-      junk <- prep(rec_obj2, training = d2_train)
-    ),
-    regexp = "[koopa: 61]"
+# Initialze a new object
+step_hcai_missing_new <- function(terms = NULL,
+                                  role = NA,
+                                  trained = FALSE,
+                                  na_percentage = NULL) {
+  step(
+    subclass = "hcai_missing",
+    terms = terms,
+    role = role,
+    trained = trained,
+    na_percentage = na_percentage
   )
-})
+}
 
-test_that("tidy method prints correctly", {
-  exp <- tibble::tibble(terms = c("character", "suit"),
-                        value = c(33.20, 8.71))
-  expect_equal(
-    exp,
-    broom::tidy(rec_obj$steps[[1]])
+#' @export
+prep.step_hcai_missing <- function(x, training, info = NULL, ...) {
+  col_names <- recipes::terms_select(terms = x$terms, info = info)
+  na_percentage <- sapply(training[, col_names], function(x) {
+    100 * sum(is.na(x)) / length(x)
+  }
   )
-})
+
+  # Give warnings about greater than 50% null
+  if (any(na_percentage > 50)) {
+    message("The following categorical columns have greater than 50% missing values
+            and will be filled with the category 'hcai_missing':")
+    message(paste0(names(na_percentage[na_percentage > 50]),
+                   ": ",
+                   round(na_percentage[na_percentage > 50], 2), "%",
+                   collapse = "\n"))
+  }
+
+  step_hcai_missing_new(
+    terms = x$terms,
+    role = x$role,
+    trained = TRUE,
+    na_percentage = na_percentage
+  )
+}
+
+#' @importFrom tidyr replace_na
+#' @importFrom stats setNames
+#' @export
+bake.step_hcai_missing <- function(object, newdata, ...) {
+  vars <- names(object$na_percentage)
+
+  # Add new level to all factors
+  newdata[vars] <- lapply(newdata[vars], function(x){
+    levels(x) <- c(levels(x), "hcai_missing")
+    x
+  })
+
+  # Replace NAs
+  replacement_list <-
+    rep("hcai_missing", length(vars)) %>%
+    as.list %>%
+    setNames(vars)
+  newdata <- newdata %>%
+    replace_na(replacement_list)
+}
+
+#' @importFrom utils getFromNamespace
+#' @export
+print.step_hcai_missing <-
+  function(x, width = max(20, options()$width - 30), ...) {
+    printer <- getFromNamespace("printer", "recipes")
+    cat("Filling NA with hcai_missing for ", sep = "")
+    printer(names(x$na_percentage), x$terms, x$trained, width = width)
+    invisible(x)
+  }
+
+#' @importFrom tibble tibble
+#' @importFrom broom tidy
+#' @export
+tidy.step_hcai_missing <- function(x, ...) {
+  if (x$trained == TRUE) {
+    res <- tibble(terms = names(x$na_percentage),
+                  value = round(x$na_percentage, 2))
+  }
+  res
+}
