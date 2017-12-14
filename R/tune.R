3d3aeaeedfca6d5ba33b3cf59d7f743880ddbba9
Michael Levy <michael.levy@healthcatalyst.com>
2017-12-13 17:13:52 -0700

    Minimal cross validation working

--- R/tune.R
+++ R/tune.R
@@ -11,11 +11,11 @@
 #' See \code{\link{supported_models}} for available models.
 #' @param n_folds How many folds to use in cross-validation? Default = 5.
 #' @param tune_depth How many hyperparameter combinations to try? Defualt = 10.
-#' @param tune_method How to search hyperparameter space? One of "random"
-#' (default) or "grid".
+#' @param tune_method How to search hyperparameter space? Only "random" is
+#' currently supported. Eventually, "random" (default) or "grid".
 #' @param metric What metric to use to assess model performance? Defaults to
 #' RMSE for model_class = regression and AUROC for model_class = classification.
-#' @param hyperparameters Optional, overrides \code{tune_depth} and
+#' @param hyperparameters Optional, a list of overrides \code{tune_depth} and
 #' \code{tune_method} if provided. A list of lists. The names of the outer-list
 #' must match \code{models}. The names of each inner-list must match the
 #' hyperparameters available to tune over for the respective model. Entries
@@ -27,60 +27,96 @@
 #' @return A model_list object
 #' @export
 #'
+#' @importFrom kknn kknn
+#' @importFrom randomForest randomForest
+#'
 #' @details Note that in general a model is trained for each hyperparameter
 #' combination in each fold for each model, so run time is a function of
 #' length(models) x n_folds x tune_depth.
 #'
-#' @examples
-#' tune()
-tune <- function(d,
-                 outcome,
-                 model_class,
-                 models = c("RF", "lasso"),
-                 n_folds = 5,
-                 tune_depth = 10,
-                 tune_method = "random",
-                 metric,
-                 hyperparameters) {
+tune_models <- function(d,
+                        outcome,
+                        model_class,
+                        models = c("rf", "knn"),
+                        n_folds = 5,
+                        tune_depth = 10,
+                        tune_method = "random",
+                        metric,
+                        hyperparameters) {
 
+  # Organize arguments and defaults
   outcome <- rlang::enquo(outcome)
-
   if (missing(metric)) {
     metric <-
       if (model_class == "regression") {
         "RMSE"
       } else if (model_class == "classification") {
-        "AUROC"
+        "ROC"
       }
   }
-
-  if (missing(hyperparameters)) {
-    # hyperparameters <- create_hyperparameters(models, tune_depth, n_var = ncol(d) - 1)
-    hyperparameters <- list(rf = list(mtry = floor(c())))
+  # We use kknn and ranger, but user input is "knn" and "rf
+  models[models == "knn"] <- "kknn"
+  models[models == "rf"] <- "ranger"
+  # Make sure outcome's class works with model_class
+  outcome_class <- class(dplyr::pull(d, !!outcome))
+  if (outcome_class %in% c("character", "factor") &&
+      model_class == "regression") {
+    stop(rlang::quo_name(outcome), " is ", outcome_class, " but you're ",
+         "trying to train a regression model.")
+  } else if (is.numeric(dplyr::pull(d, !!outcome)) &&
+             model_class == "classification") {
+    stop(rlang::quo_name(outcome), " is ", outcome_class, " but you're ",
+         "trying to train a classification model. If that's what you want ",
+         "convert it explicitly with as.factor().")
   }
 
-  if (tune_method = "grid") {
-    tune_grid <- do.call(expand.grid, hyperparameters)
+  if (tune_method == "random") {
+    train_control <-
+      caret::trainControl(method = "cv",
+                          number = n_folds,
+                          search = "random"
+      )
+  } else {
+    stop("Currently tune_method = \"random\" is the only supported method",
+         " but you supplied tune_method = \"", tune_method, "\"")
+    #### Not needed for only random search
+    # if (missing(hyperparameters)) {
+    #   # hyperparameters <- create_hyperparameters(models, tune_depth, n_var = ncol(d) - 1)
+    #   hyperparameters <- list(rf = list(mtry = floor(c())))
+    # }
+
+    # if (tune_method == "grid") {
+    #   tune_grid <- do.call(expand.grid, hyperparameters)
+    #   train_control <-
+    #     caret::trainControl(method = "cv",
+    #                         number = n_folds,
+    #                         search = "grid"
+    #                         )
+    #   #### Grid is provided in train
+    #
+    # }
   }
 
-  train_control <-
-    caret::trainControl(method = "cv",
-                        number = n_folds,
-                        summaryFunction = twoClassSummary,
-                        search = tune_method)
+  # trainControl defaults are good for regression. Change for other model_class
+  if (model_class == "classification") {
+    train_control$summaryFunction <- twoClassSummary  # nolint
+    train_control$classProbs <- TRUE  # nolint
+  }
 
   train_list <-
     lapply(models, function(model) {
-
-      caret::train(x = dplyr::select(d, !!outcome),
+      message("Running cross validation for ", model)
+      caret::train(x = dplyr::select(d, -!!outcome),
                    y = dplyr::pull(d, !!outcome),
-                   method = "rf",
-                   tuneLength = tune_depth,
+                   method = model,
                    metric = metric,
-                   ntree = c(50, 100)
+                   trControl = train_control,
+                   tuneLength = tune_depth
       )
+    })
 
-
-  })
-
+  class(train_list) <- c(paste0(model_class, "_list"),
+                         "model_list",
+                         class(train_list))
+  return(train_list)
 }
--- tests/testthat/test-tune.R
+++ tests/testthat/test-tune.R
@@ -1,17 +1,73 @@
 context("Checking model tuning")
 
 # Setup ------------------------------------------------------------------------
-test_df <- tibble::tibble(
+test_df <- data.frame(
   x1 = rep(letters[1:2], each = 10),
   x2 = rnorm(20),
   x3 = c(1:19, 25)
 )
 
-test_that("tune doesn't error on simple regression or classification use", {
-  expect_error(tune(test_df, x3, "regression", "lasso", 1, 1), NA)
-  expect_error(tune(test_df, x1, "classification", "RF", 1, 1), NA)
+## Temporary test until grid search is implemented
+test_that("tune_models errors if tune_method isn't 'random'", {
+  expect_error(tune_models(test_df, x3, "regression", tune_method = "grid"),
+               "random")
 })
 
-test_that("tune returns a model_list", {
-  expect_s3_class(tune(test_df, x3, "regression", "lasso", 1, 1), "model_list")
+test_that("Error informatively if outcome class doesn't match model_class", {
+  expect_error(tune_models(test_df, x1, "regression"), class(test_df$x1))
+  test_df$x1 <- as.character(test_df$x1)
+  expect_error(tune_models(test_df, x1, "regression"), class(test_df$x1))
+  expect_error(tune_models(test_df, x3, "classification"), class(test_df$x3))
+})
+
+# No error for each algorithm x response-class type
+test_that("tune_models doesn't error on knn regression", {
+  expect_error(
+    tune_models(d = test_df, outcome = x3, model_class = "regression",
+                models = "knn", n_folds = 2, tune_depth = 2)
+    , regexp = NA)
+
+})
+
+test_that("tune_models doesn't error on knn classification", {
+  expect_error(
+    tune_models(d = test_df, outcome = x1, model_class = "classification",
+                models = "knn", n_folds = 2, tune_depth = 2)
+    , regexp = NA)
+
+})
+
+test_that("tune_models doesn't error on rf regression", {
+  expect_error(
+    tune_models(d = test_df, outcome = x3, model_class = "regression",
+                models = "rf", n_folds = 2, tune_depth = 2)
+    , regexp = NA)
+
+})
+
+test_that("tune_models doesn't error on rf classification", {
+  expect_error(
+    tune_models(d = test_df, outcome = x1, model_class = "classification",
+                models = "rf", n_folds = 2, tune_depth = 2)
+    , regexp = NA)
+})
+
+test_that("tune_models doesn't error on rf & knn classification", {
+  expect_error(
+    tune_models(d = test_df, outcome = x1, model_class = "classification",
+                models = c("rf", "knn"), n_folds = 2, tune_depth = 2)
+    , regexp = NA)
+})
+
+test_that("tune_models returns a model_list", {
+  c_models <-
+    tune_models(d = test_df, outcome = x1, model_class = "classification",
+                n_folds = 2, tune_depth = 2)
+  r_models <-
+    tune_models(d = test_df, outcome = x3, model_class = "regression",
+                n_folds = 2, tune_depth = 2)
+  expect_s3_class(c_models, "model_list")
+  expect_s3_class(c_models, "classification_list")
+  expect_s3_class(r_models, "model_list")
+  expect_s3_class(r_models, "regression_list")
 })
