fe2c91ce062166a2d4f3248731c68b49f2a54343
Mike Mastanduno <michael.mastanduno@healthcatalyst.com>
2017-12-28 15:33:53 -0700

    Changed to quo

--- R/impute.R
+++ R/impute.R
@@ -5,11 +5,9 @@
 #' both nominal and numeric data. Currently supports mean (numeric only),
 #' new_category (categorical only), bagged trees, or knn. Grain and target will
 #' not be imputed.
-#' @param data A dataframe or tibble containing data to impute.
-#' @param target A string, the name of the target column. Required.
-#' @param grain A string, the name of the grain column. Required.
+#' @param d A dataframe or tibble containing data to impute.
 #' @param rec_obj Optional, a recipe object. If provided, this recipe will be
-#' used to bake data contained in data.
+#' used to bake data contained in d.
 #' @param numeric_method Defaults to \code{"mean"}. Other choices are
 #' \code{"bagimpute"} or \code{"knnimpute"}.
 #' @param nominal_method Defaults to \code{"new_category"}. Other choices are
@@ -45,25 +43,25 @@
 #' d_train <- d[1:80, ]
 #' d_test <- d[81:100, ]
 #' # Train imputer and apply
-#' data_and_recipe <- impute(data = d_train,
+#' data_and_recipe <- impute(d = d_train,
 #'                           grain = "patient_id",
 #'                           target = "disease")
 #'
 #' # Apply to new data
-#' res <- impute(data = d_test,
+#' res <- impute(d = d_test,
 #'               grain = "patient_id",
 #'               target = "disease",
 #'               rec_obj = data_and_recipe$rec_obj)
 #'
 #' # Specify methods:
-#' data_and_recipe <- impute(data = d_train,
+#' d_and_recipe <- impute(d = d_train,
 #'                           grain = "patient_id",
 #'                           target = "disease",
 #'                           numeric_method = "bagimpute",
 #'                           nominal_method = "new_category")
 #'
 #' # Specify method and param:
-#' data_and_recipe <- impute(data = d_train,
+#' d_and_recipe <- impute(d = d_train,
 #'                           grain = "patient_id",
 #'                           target = "disease",
 #'                           nominal_method = "knnimpute",
@@ -72,17 +70,16 @@
 #'
 #'
 #'
-impute <- function(data = NULL,
-                   target = NULL,
-                   grain = NULL,
+impute <- function(d = NULL,
                    rec_obj = NULL,
+                   ignore_columns,
                    numeric_method = "mean",
                    nominal_method = "new_category",
                    numeric_params = NULL,
                    nominal_params = NULL) {
-  # Check to make sure that df is a dataframe
-  if (!(is.data.frame(data))) {
-    stop("\"data\" must be a tibble or dataframe.")
+  # Check to make sure that df is a dframe
+  if (!(is.data.frame(d))) {
+    stop("\"d\" must be a tibble or dframe.")
   }
 
   # Check to make sure rec_obj is a valid recipe
@@ -90,50 +87,47 @@ impute <- function(data = NULL,
     stop("\"rec_obj\" must be a valid recipe object.")
   }
 
-  if (is.null(target) || !(target %in% names(data))) {
-    stop("\"target\" must be a column name in data")
-  }
-  if (is.null(grain) || !(grain %in% names(data))) {
-    stop("\"grain\" must be a column name in data.")
-  }
+  ignore_columns <- rlang::enquo(ignore_columns)
 
-  d_grain <- data[grain]
-  d_target <- data[target]
+  if (rlang::quo_is_missing(ignore_columns)) {
+    d_ignore <- NULL
+  } else {
+    # Make sure all columns are present
+    cols <- sapply(ignore_columns, rlang::quo_name)
+    present <- cols %in% names(d)
+    if (any(!present))
+      stop(paste(cols[!present], collapse = ", "), " not found in d.")
+
+    d_ignore <- select(d, !!ignore_columns)
+    d <- select(d, -!!ignore_columns)
+  }
 
   # Save column order
-  col_order <- names(data)
+  col_order <- names(d)
 
-  message("Data contains the following levels of missingness:")
-  print(missingness(data))
-  message(paste0("Grain and target columns will not be imputed. These rows ",
-    "should be removed before training a model!"))
-
-  # Recipes will impute grain even if it's removed in formula. Remove it
-  # explicitly. Target will be removed by formula.
-  data[grain] <- NULL
+  message("d contains the following levels of missingness:")
+  print(missingness(d))
+  message(paste0("Ignore columns will not be imputed."))
 
   # If recipe object is not provided, train it and predict.
   if (is.null(rec_obj)) {
-    form = paste0(target, " ~ .")
-
     # Train
-    rec_obj <- recipe(x = data, formula = form) %>%
+    rec_obj <- recipe(x = d) %>%
       hcai_impute(numeric_method = numeric_method,
                   nominal_method = nominal_method,
                   numeric_params = numeric_params,
                   nominal_params = nominal_params) %>%
-        prep(training = data)
+        prep(training = d)
   }
     # Predict
-  data_imputed <-
-    bake(rec_obj, newdata = data)
+  d_imputed <-
+    bake(rec_obj, newdata = d)
 
-  # Add grain and target back in.
-  data_imputed[grain] <- d_grain
-  data_imputed[target] <- d_target
-  data_imputed <- data_imputed[, col_order]
+  # Add ignore columns back in.
+  d_imputed <- bind_cols(d_imputed, d_ignore)
+  d_imputed <- d_imputed[, col_order]
 
-  return(list(data_imputed = data_imputed,
+  return(list(d_imputed = d_imputed,
             rec_obj = rec_obj))
 }
 
