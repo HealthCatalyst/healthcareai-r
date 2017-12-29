38dbec96de7ae8fc11e39e018594587081a29a0b
Michael Levy <michael.levy@healthcatalyst.com>
2017-12-28 18:40:03 -0700

    skeletons for regression_list

---
+++ R/regression_list.R
@@ -0,0 +1,16 @@
+plot.regression_list <- function(rlist) {
+
+}
+
+summary.regression_list <- function(rlist) {
+
+}
+
+print.regression_list <- function(rlist) {
+
+}
+
+evaluate.regression_list <- function(rlist) {
+
+  return()  # Best model
+}
