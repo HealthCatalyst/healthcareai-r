07871fdf230df3b60d7daaf702f3f69c94d92b24
Michael Levy <michael.levy@healthcatalyst.com>
2017-12-29 14:55:52 -0700

    function to extract info from model_list

--- R/regression_list.R
+++ R/regression_list.R
@@ -9,6 +9,7 @@
 #' @export
 #' @importFrom cowplot plot_grid
 #' @importFrom purrr map_df
+#' @importFrom purrr map_chr
 #'
 #' @examples
 #' m <- tune(mtcars, mpg)
@@ -36,23 +37,52 @@ plot.regression_list <- function(rlist, print = TRUE) {
   return(invisible(gg))
 }
 
-summary.regression_list <- function(rlist) {
-  # Data details: rows, features, outcome class/alg type
-  # CV Details: n-folds, param depth
-  # Names of algs
-  # Best performing alg and hyperparameter values
-  # Tables of hyperparamter values and performance
-  names(m[[1]])
-  m[[1]]$trainingData
-
-}
-
-print.regression_list <- function(rlist) {
-  # Algs trained, depth
-  # Best model: Alg, hyperparameters, metric
-}
+# summary.regression_list <- function(rlist) {
+#   # Data details: rows, features, outcome class/alg type
+#   # CV Details: n-folds, param depth
+#   # Names of algs
+#   # Best performing alg and hyperparameter values
+#   # Tables of hyperparamter values and performance
+#   names(m[[1]])
+#   print()
+#   ncol(m[[1]]$trainingData) - 1
+#
+# }
+#
+# print.regression_list <- function(rlist) {
+#   rinfo <- extract_model_info(rlist)
+#   cat("Trained", tolower(rinfo$m_class),
+#       tolower(paste(rinfo$algs, collapse = ", ")), "on"
+#   )
+#
+# }
 
 evaluate.regression_list <- function(rlist) {
 
   return()  # Best model
+}
+
+extract_model_info <- function(mlist) {
+  # optimum is min or max depending on metric
+  optimum <- if (mlist[[1]]$maximize) max else min
+  metric <- mlist[[1]]$metric
+  best_metrics <- purrr::map_dbl(mlist, ~ optimum(.x$results[[metric]]))
+  best_model <- which(best_metric == optimum(best_metric))[1] # 1 in case of tie
+  algs <- purrr::map_chr(mlist, ~ .x$modelInfo$label)
+  m_class <- mlist[[1]]$modelType
+  # outcome <-
+    mlist[[1]]$finalModel$
+  ddim <- dim(mlist[[1]]$trainingData)
+  best_model_name <- algs[best_model]
+  best_model_perf <- best_metrics[best_model]
+  best_model_tune <- mlist[[best_model]]$bestTune
+  list(
+    m_class = m_class,
+    algs = algs,
+    metric = metric,
+    best_model_name = best_model_name,
+    best_model_perf = best_model_perf,
+    best_model_tune = best_model_tune,
+    ddim = ddim
+  )
 }
