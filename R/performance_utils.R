63d992d0c85b37175dce5bdad7b56ddf23921382
Mike Mastanduno <michael.mastanduno@healthcatalyst.com>
2018-02-12 09:53:59 -0700

    MLmetrics to namespace (used by caret function)

--- R/performance_utils.R
+++ R/performance_utils.R
@@ -1,9 +1,16 @@
 #' Copy of the caret function, `prSummary`, so that we can change the output
 #' param to "PR" instead of "AUC." Stated Precision and Recall assume a cutoff of 0.5
 #' @noRd
-pr_summary <- function (data, lev = NULL, model = NULL)
-{
-  caret:::requireNamespaceQuietStop("MLmetrics")
+#' @importFrom MLmetrics PRAUC
+#' @importFrom utils getFromNamespace
+pr_summary <- function (data, lev = NULL, model = NULL) {
+  # Non-exported functions
+  precision.default <- getFromNamespace("precision.default", "caret")
+  recall.default <- getFromNamespace("recall.default", "caret")
+  F_meas.default <- getFromNamespace("F_meas.default", "caret")
+  requireNamespaceQuietStop <- getFromNamespace("requireNamespaceQuietStop", "caret")
+
+  requireNamespaceQuietStop("MLmetrics")
   if (length(levels(data$obs)) > 2)
     stop(paste("Your outcome has", length(levels(data$obs)),
                "levels. `prSummary`` function isn't appropriate.",
@@ -16,13 +23,13 @@ pr_summary <- function (data, lev = NULL, model = NULL)
   if (inherits(pr_auc, "try-error"))
     pr_auc <- NA
   c(PR = pr_auc,
-    Precision = caret:::precision.default(data = data$pred,
+    Precision = precision.default(data = data$pred,
                                           reference = data$obs,
                                           relevant = lev[1]),
-    Recall = caret:::recall.default(data = data$pred,
+    Recall = recall.default(data = data$pred,
                                     reference = data$obs,
                                     relevant = lev[1]),
-    F = caret:::F_meas.default(data = data$pred,
+    F = F_meas.default(data = data$pred,
                                reference = data$obs,
                                relevant = lev[1]))
 }
