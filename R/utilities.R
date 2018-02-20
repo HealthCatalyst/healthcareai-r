ad4773d6748b0fdc769e9ea0504659eaa49842f0
Michael Levy <michael.levy@healthcatalyst.com>
2018-02-19 20:23:04 -0700

    Revert "add stopping checks for unhandled missingness or new levels in predict"
    This reverts commit 8cdc3d45b68cb3d51cddcb6627ee39f61754dfce.

--- R/utilities.R
+++ R/utilities.R
@@ -29,31 +29,3 @@ skip_on_not_appveyor <- function() {
     }
     testthat::skip("Not on Appveyor")
 }
-
-#' find_new_levels
-#'
-#' @return A list, of length zero if all observed character values/factor levels
-#'   in new_df were observed in ref_df, otherwise with names of variable names
-#'   and character vectors of previously unobserved values.
-#' @noRd
-find_new_levels <- function(new_df, ref_df) {
-  levs <-
-    dplyr::select_if(new_df, ~ !is.numeric(.x)) %>%
-    purrr::map(~ unique(.x))
-  purrr::map(names(levs), ~
-    as.character(levs[[.x]][!levs[[.x]] %in% unique(ref_df[[.x]])])) %>%
-    setNames(names(levs))
-}
-
-#' Take list of character vectors as from find_new_levels and format for
-#' printing
-#'
-#' @return character vector
-#' @noRd
-format_new_levels <- function(new_levels) {
-  purrr::map(names(new_levels), ~ {
-    if (!length(new_levels[[.x]])) return(NULL) else
-      paste0("\n\t", .x, ": ", paste(new_levels[[.x]], collapse = ", "))
-  }) %>%
-    purrr::flatten()
-}
