d95fa9a681fd9f97fd69b3a28f1dc8a3c65e521b
Mike Mastanduno <michael.mastanduno@healthcatalyst.com>
2018-03-06 12:06:26 -0700

    last test

--- tests/testthat/test-db-connections.R
+++ tests/testthat/test-db-connections.R
@@ -32,20 +32,12 @@ test_that("trusted false throws error with no uid", {
 test_that("connection can be made using built string", {
   # skip_on_not_appveyor()
   cs <- build_connection_string(server = "localhost", database = "testSAM")
-
-csmanual = '
-  driver={SQL Server};
-  server=localhost;
-  database=testSAM;
-  trusted_connection=true'
-
   con <- DBI::dbConnect(odbc::odbc(), .connection_string = cs)
   dd <- DBI::dbGetQuery(con, "select * from testSAM.dbo.hcai_unit_tests")
   expect_equal(names(dd)[1], "id")
   expect_equal(names(dd)[2], "word_of_day")
   expect_equal(dd$word_of_day[1], "bagel")
-  stop(paste("class msg is:", class(con)))
-  # expect_equal(print(class(con)), "Microsoft SQL Server")
+  expect_equal(print(class(con)), "Microsoft SQL Server")
   DBI::dbDisconnect(con)
 })
 
