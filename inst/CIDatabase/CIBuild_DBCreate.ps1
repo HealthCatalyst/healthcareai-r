859718d3693134935e33887cccf32926c7b4250c
Mike Mastanduno <michael.mastanduno@healthcatalyst.com>
2018-03-06 09:46:41 -0700

    added some gos

--- inst/CIDatabase/CIBuild_DBCreate.ps1
+++ inst/CIDatabase/CIBuild_DBCreate.ps1
@@ -1,10 +1,12 @@
-Invoke-Sqlcmd -ServerInstance "(local)\SQL2012SP1" -Query "
+Invoke-Sqlcmd -ServerInstance 'localhost' -Query "
 CREATE DATABASE testSAM
+GO
 
 CREATE TABLE testSAM.dbo.hcai_unit_tests(
 	id INT NULL,
 	word_of_day varchar(255) NULL
 )
+GO
 
 INSERT INTO testSAM.dbo.hcai_unit_tests VALUES
 (1, 'bagel'),
