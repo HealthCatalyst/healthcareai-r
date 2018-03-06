50883940df085e75a72bb0e4293d7992822433e4
Mike Mastanduno <michael.mastanduno@healthcatalyst.com>
2018-03-06 10:02:28 -0700

    try to run test.

--- inst/CIDatabase/CIBuild_DBCreate.ps1
+++ inst/CIDatabase/CIBuild_DBCreate.ps1
@@ -1,1 +1,16 @@
-sqlcmd -b -S "(local)\SQL2012SP1" -Q "CREATE DATABASE testSAM"
+sqlcmd -b -S "(local)\SQL2012SP1" -Q "
+CREATE DATABASE testSAM
+GO
+
+CREATE TABLE testSAM.dbo.hcai_unit_tests(
+	id INT NULL,
+	word_of_day varchar(255) NULL
+)
+GO
+
+INSERT INTO testSAM.dbo.hcai_unit_tests VALUES
+(1, 'bagel'),
+(2, 'box'),
+(3, 'toaster')
+"
+
