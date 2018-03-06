7c782b54f42435b77ee8672c5d3aeb2e3121b987
Mike Mastanduno <michael.mastanduno@healthcatalyst.com>
2018-03-06 09:50:49 -0700

    just create db

--- inst/CIDatabase/CIBuild_DBCreate.ps1
+++ inst/CIDatabase/CIBuild_DBCreate.ps1
@@ -1,15 +1,1 @@
-Invoke-Sqlcmd -ServerInstance 'localhost' -Query "
-CREATE DATABASE testSAM
-GO
-
-CREATE TABLE testSAM.dbo.hcai_unit_tests(
-	id INT NULL,
-	word_of_day varchar(255) NULL
-)
-GO
-
-INSERT INTO testSAM.dbo.hcai_unit_tests VALUES
-(1, 'bagel'),
-(2, 'box'),
-(3, 'toaster')
-"
+sqlcmd -b -S "(local)\SQL2012SP1" -Q "CREATE DATABASE testSAM"
