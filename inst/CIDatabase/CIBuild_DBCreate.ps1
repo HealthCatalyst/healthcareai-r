7b6345020506950c4229c14d06fab0cdb368da5f
Mike Mastanduno <michael.mastanduno@healthcatalyst.com>
2018-03-07 15:15:32 -0700

    adding other schema

--- inst/CIDatabase/CIBuild_DBCreate.ps1
+++ inst/CIDatabase/CIBuild_DBCreate.ps1
@@ -12,5 +12,24 @@ INSERT INTO testSAM.dbo.hcai_unit_tests VALUES
 (1, 'bagel'),
 (2, 'box'),
 (3, 'toaster')
+GO
+
+
+
+USE testSAM
+GO
+
+CREATE SCHEMA test_schema
+	CREATE TABLE hcai_unit_tests(
+		id INT NULL,
+		word_of_day varchar(255) NULL
+	)
+GO
+
+INSERT INTO testSAM.test_schema2.hcai_unit_tests VALUES
+	(4, 'lentil'),
+	(5, 'automobile'),
+	(6, 'towel')
+GO
 "
 
