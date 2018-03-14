f5a869bf0a66fbdec4b56332d5af7a8fd0821247
Mike Mastanduno <michael.mastanduno@healthcatalyst.com>
2018-03-14 08:48:31 -0600

    Refactoring and cleaning old functions
    Build and Read are in db_connections.
    Tests are in test-db-connections.
    All write functions were removed.

--- tests/testthat/test-db-connections.R
+++ tests/testthat/test-db-connections.R
@@ -57,3 +57,66 @@ test_that("connection can be made using built string and dbplyr", {
   expect_equal(class(con)[1], "Microsoft SQL Server")
   DBI::dbDisconnect(con)
 })
+
+
+# Read from Database ---------------------------
+context("Checking that write to database works")
+library(dplyr)
+
+cs <- build_connection_string(server = "localhost",
+                              database = "testSAM")
+
+
+test_that("con is a connection", {
+  expect_error(res <- db_read("fake_con",
+                              "SELECT * FROM testSAM.dbo.hcai_unit_tests"),
+               "SQL Server")
+})
+
+test_that("data reads using sql string", {
+  skip_on_not_appveyor()
+  con <- DBI::dbConnect(odbc::odbc(), .connection_string = cs)
+  res <- db_read(con, "SELECT * FROM testSAM.dbo.hcai_unit_tests")
+  exp <- tibble::tibble(id = 1:3,
+                        word_of_day = c("bagel", "box", "toaster"))
+  expect_equal(res, exp)
+  DBI::dbDisconnect(con)
+})
+
+test_that("data reads special schema using sql string", {
+  skip_on_not_appveyor()
+  con <- DBI::dbConnect(odbc::odbc(), .connection_string = cs)
+  res <- db_read(con, "SELECT * FROM testSAM.test_schema.hcai_unit_tests")
+  exp <- tibble::tibble(id = 4:6,
+                        word_of_day = c("lentil", "automobile", "towel"))
+  expect_equal(res, exp)
+  DBI::dbDisconnect(con)
+})
+
+test_that("data reads a pointer if not collected", {
+  skip_on_not_appveyor()
+  con <- DBI::dbConnect(odbc::odbc(), .connection_string = cs)
+  pt <- db_read(con,
+                query = "SELECT * FROM testSAM.dbo.hcai_unit_tests",
+                pull_into_memory = FALSE)
+  res <- pt %>% collect()
+  exp <- tibble::tibble(id = 1:3,
+                        word_of_day = c("bagel", "box", "toaster"))
+  expect_equal(class(pt)[1], "tbl_dbi")
+  expect_equal(res, exp)
+  DBI::dbDisconnect(con)
+})
+
+test_that("data reads a pointer from special schema if not collected", {
+  skip_on_not_appveyor()
+  con <- DBI::dbConnect(odbc::odbc(), .connection_string = cs)
+  pt <- db_read(con,
+                query = "SELECT * FROM testSAM.test_schema.hcai_unit_tests",
+                pull_into_memory = FALSE)
+  res <- pt %>% collect()
+  exp <- tibble::tibble(id = 4:6,
+                        word_of_day = c("lentil", "automobile", "towel"))
+  expect_equal(class(pt)[1], "tbl_dbi")
+  expect_equal(res, exp)
+  DBI::dbDisconnect(con)
+})
