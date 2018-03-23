57ef82e60309bcb58c105023e1477d1b22dfa19e
Michael Levy <michael.levy@healthcatalyst.com>
2018-03-23 17:10:07 -0600

    plot regression predictions function

---
+++ R/plot_predictions.R
@@ -0,0 +1,95 @@
+#' Plot model predictions vs observed outcomes
+#'
+#' @param x data frame as returned `predict.model_list`
+#' @param outcomes Vector of outcomes if not present in x
+#' @param print Logical, if TRUE (default) the plot is printed on the current
+#'   graphics device. The plot is always (silently) returned.
+#' @param ... Parameters to pass to plot_regression_predictions or
+#'   plot_classification_predictions
+#'
+#' @return A ggplot object
+#' @export
+#'
+#' @details See additional arguments that can be passed through in
+#'   \code{\link{plot_regression_predictions}} and
+#'   \code{\link{plot_classification_predictions}}
+#'
+#'
+#' @examples
+#' models <- machine_learn(pima_diabetes[1:50, ], patient_id, outcome = plasma_glucose)
+#' predictions <- predict(models)
+#' plot(predictions)
+#' plot(predictions, title = "This model's predictions regress to the mean",
+#'      point_size = 3, point_alpha = .7, font_size = 14)
+plot.hcai_predicted_df <- function(x, outcomes = NULL, print = TRUE, ...) {
+
+  # Checks, and put outcomes in x if necessary
+  mi <- attr(x, "model_info")
+  target <- mi$target
+  if (!is.null(outcomes)) {
+    #  outcomes provided
+    if (target %in% names(x))
+      warning(target, " is present in `x`, but you passed values to `outcomes`. ",
+              "The values in x will be ignored.")
+    if(length(outcomes) != nrow(x))
+      stop("There are ", nrow(x), " rows in `x` but ", length(outcomes), " values in `outcomes`. ",
+           "These must be the same length.")
+    x[[target]] <- outcomes
+  } else {
+    if (!target %in% names(x))
+      stop(target, " is not present in `x`, so you must pass a vector of outcomes to `outcomes`.")
+  }
+
+  # Check outcome type and dispatch the plotting function for the model type
+  if (mi$type == "Regression") {
+    if (!is.numeric(x[[target]]))
+      stop("x comes from a regression model, but `outcomes` is class ", class(outcomes))
+    the_plot <- plot_regression_predictions(x, target = target, ...)
+  } else if (mi$type == "Classification") {
+    distinct_outcomes <- length(unique(x[[target]]))
+    if (distinct_outcomes > 2)
+      stop("x comes from a classification model, but there are ",
+           distinct_outcomes, " distinct outcomes in `outcomes`.")
+    the_plot <- plot_classification_predictions(x, target = target, ...)
+  } else {
+    stop("Plotting predictions of model type ", mi$type, "is not currently supported.")
+  }
+
+  # Print and return
+  if (print)
+    print(the_plot)
+  return(invisible(the_plot))
+}
+
+#' Plot predictions from a regression model
+#'
+#' @param x hcai_predicted_df from plot.hcai_predicted_df
+#' @param title Character: Plot title
+#' @param point_size Number: Point size, relative to 1
+#' @param point_alpha Number in [0, 1] giving point opacity
+#' @param font_size Relative size of all font in plot
+#' @param target outcome column name
+#'
+#' @return ggplot object
+#'
+#' @details This function is not meant to be called directly. Use
+#'   \code{plot(predictions)} instead. You can pass arguments to this function
+#'   (e.g. a plot title) through \code{plot}.
+plot_regression_predictions <- function(x,
+                                        title = NULL,
+                                        point_size = 1,
+                                        point_alpha = 1,
+                                        font_size = 11,
+                                        target) {
+  preds <- paste0("predicted_", target)
+  limits <- range(c(x[[target]]), x[[preds]])
+  p <-
+    ggplot(x, aes_string(x = target, y = preds)) +
+    geom_abline(linetype = "dashed", alpha = .6)  +
+    geom_point(size = point_size, alpha = point_alpha) +
+    scale_x_continuous(name = paste("Actual ", target), limits = limits) +
+    scale_y_continuous(name = paste("Predicted ", target), limits = limits) +
+    ggtitle(title) +
+    theme_gray(base_size = font_size)
+  return(p)
+}
---
+++ tests/testthat/test-plot_predictions.R
@@ -0,0 +1,38 @@
+context("Checking prediction plots")
+
+### Setup
+d <- prep_data(pima_diabetes[1:100, ], patient_id, outcome = age)
+m <- tune_models(d, outcome = age, n_folds = 2, tune_depth = 1, models = "rf")
+reg_preds_self <- predict(m)
+reg_preds_new <- predict(m, pima_diabetes[101:110, ])
+
+### Tests
+test_that("plot.hcai_predicted_df stops if there's no outcome", {
+  expect_error(plot(dplyr::select(reg_preds_self, -age)), "outcome")
+  expect_error(plot(dplyr::select(reg_preds_new, -age)), "outcome")
+})
+
+test_that("plot.hcai_predicted_df stops if outcome vector is wrong length or class", {
+  expect_error(plot(dplyr::select(reg_preds_self, -age), outcomes = 1:5),
+               "length")
+  expect_error(plot(dplyr::select(reg_preds_self, -age),
+                    outcomes = sample(letters, nrow(reg_preds_self), TRUE),
+                    print = FALSE),
+               "class")
+})
+
+test_that("plot.hcai_predicted_df warns but works if outcomes present in df and passed in", {
+  expect_warning(p <- plot(reg_preds_new, 1:10, print = FALSE), "outcome")
+  expect_s3_class(p, "gg")
+})
+
+test_that("plot_regression_predictions handles defaults", {
+  expect_s3_class(plot(reg_preds_new), "gg")
+  expect_s3_class(plot(reg_preds_self), "gg")
+})
+
+test_that("plot_regression_predictions handles separately supplied outcomes", {
+  expect_s3_class(plot(dplyr::select(reg_preds_self, -age),
+                       outcomes = reg_preds_self$age, print = FALSE),
+                  "gg")
+})
