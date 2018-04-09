37147fae2de7997399a84438c2ce8c163cf6a0f9
Michael Levy <michael.levy@healthcatalyst.com>
2018-04-09 15:21:38 -0600

    evaluate functions. Closes #1004

--- R/evaluate.R
+++ R/evaluate.R
@@ -1,15 +1,43 @@
-#' Get performance metrics from prediction data frame
+#' Get performance metrics from predictions data frame
 #'
-#' @param df
+#' @param d Data frame from \code{\link{predict.model_list}} containing realized
+#'   outcomes
 #'
-#' @return
+#' @return Vector of scores with metrics as names
 #' @export
+#' @details This function is designed to work with predictions coming from
+#'   \code{\link{machine_learn}} / \code{\link{tune_models}} /
+#'   \code{\link{flash_models}} through \code{\link{predict.model_list}}. The
+#'   data passed to \code{predict} must contain observed outcomes. If you have
+#'   predictions and outcomes in a different format, use
+#'   \code{\link{evaluate_classification}} or \code{\link{evaluate_regression}}.
 #'
 #' @examples
-evaluate.hcai_predicted_df <- function(df) {
+#' models <- machine_learn(pima_diabetes[1:40, ], patient_id, outcome = diabetes,
+#'                         models = "rf", tune_depth = 3)
+#' predictions <- predict(models, newdata = pima_diabetes[41:50, ])
+#' evaluate(predictions)
+evaluate <- function(d) {
+  outcome <- attr(d, "model_info")[["target"]]
+  if (!outcome %in% names(d))
+    stop("Outcome variable: ", outcome, " not found in d. You must have actual outcomes in ",
+         "the data passed to predict to use evaluate. If the outcomes are ",
+         "somewhere else, consider using ",
+         paste0("evaluate_", tolower(attr(d, "model_info")[["type"]])))
 
-  # if observed values not present, stop
-  # Dispatch passing vectors
+  obs <- d[[outcome]]
+  pred <- d[[paste0("predicted_", outcome)]]
+
+  if (attr(d, "model_info")[["type"]] == "Regression") {
+    scores <- evaluate_regression(predicted = pred, actual = obs)
+  } else if (attr(d, "model_info")[["type"]] == "Classification") {
+    obs <- ifelse(obs == attr(d, "positive_class"), 1L, 0L)
+    scores <- evaluate_classification(predicted = pred, actual = obs)
+  } else {
+    stop("Somthing's gone wrong. I don't know how to deal with model type ",
+         attr(d, "model_info")[["type"]])
+  }
+  return(scores)
 }
 
 #' Get performance metrics for classification predictions
--- tests/testthat/test-evaluate.R
+++ tests/testthat/test-evaluate.R
@@ -1,4 +1,5 @@
 context("Checking evaluate")
+library(magrittr)
 
 # Setup
 d <- na.omit(pima_diabetes)[1:100, ]
@@ -7,21 +8,13 @@ dtest <- na.omit(pima_diabetes)[101:110, ]
 r_models <- machine_learn(d, patient_id, outcome = plasma_glucose, tune = FALSE, n_folds = 2)
 c_models <- machine_learn(d, patient_id, outcome = diabetes, tune = FALSE, n_folds = 2)
 
-r_preds_training <- predict(r_models)
-c_preds_training <- predict(c_models)
-r_preds_test <- predict(r_models, dtest)
-c_preds_test <- predict(c_models, dtest)
+r_preds_training <- predict(r_models) %>% evaluate()
+c_preds_training <- predict(c_models) %>% evaluate()
+r_preds_test <- predict(r_models, dtest) %>% evaluate()
+c_preds_test <- predict(c_models, dtest) %>% evaluate()
 all_preds <- list(r_preds_training, r_preds_test, c_preds_training, c_preds_test)
 
 # Test
-test_that("evaluate.hcai_predicted_df is registered generic", {
-  expect_true(any(stringr::str_detect(methods("evaluate"), "hcai_predicted_df")))
-})
-
-test_that("evaluate returns numeric vector", {
-  expect_true(all(map_lgl(all_preds, is.numeric)))
-})
-
 test_that("evalute_classification returns numeric with names being metrics", {
   eval_class <- evaluate_classification(c(.3, .7, .8), c(0, 1, 0))
   expect_true(is.numeric(eval_class))
@@ -32,4 +25,37 @@ test_that("evalute_regression returns numeric with names being metrics", {
   eval_reg <- evaluate_regression(c(.3, .7, .8), c(4, 6, 9))
   expect_true(is.numeric(eval_reg))
   expect_setequal(names(eval_reg), c("RMSE", "MAE", "Rsquared"))
+})
+
+test_that("evaluate barfs if target isn't present", {
+  expect_error(
+    dplyr::select(dtest, -plasma_glucose) %>%
+      predict(r_models, .) %>%
+      evaluate()
+    , regexp = "plasma_glucose")
+  expect_error(
+    dtest %>%
+      predict(c_models, .) %>%
+      dplyr::select(-diabetes) %>%
+      evaluate()
+    , regexp = "diabetes")
+})
+
+test_that("evaluate returns numeric vector", {
+  purrr::map_lgl(all_preds, is.numeric) %>%
+    all() %>%
+    expect_true()
+})
+
+test_that("evalutes returns appropriate metrics", {
+  expect_setequal(names(r_preds_training), c("RMSE", "MAE", "Rsquared"))
+  expect_setequal(names(r_preds_test), c("RMSE", "MAE", "Rsquared"))
+  expect_setequal(names(c_preds_training), c("AUPR", "AUROC"))
+  expect_setequal(names(c_preds_test), c("AUPR", "AUROC"))
+})
+
+test_that("All AUx metrics are in [0, 1]", {
+  map_lgl(c_preds_test, ~ .x <= 1 && .x >= 0) %>%
+    all() %>%
+    expect_true()
 })
