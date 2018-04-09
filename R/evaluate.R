8dfa77f20b296b575809fcfafb3cff87d1a7ba13
Michael Levy <michael.levy@healthcatalyst.com>
2018-04-09 14:37:03 -0600

    evaluate regression predictions function

--- R/evaluate.R
+++ R/evaluate.R
@@ -12,7 +12,7 @@ evaluate.hcai_predicted_df <- function(df) {
   # Dispatch passing vectors
 }
 
-#' Get performance metrics for classification models
+#' Get performance metrics for classification predictions
 #'
 #' @param predicted Vector of predicted probabilities
 #' @param actual Vector of realized outcomes, must be 0/1
@@ -24,13 +24,30 @@ evaluate.hcai_predicted_df <- function(df) {
 #' @export
 #'
 #' @examples
+#' evaluate_classification(c(.7, .1, .6, .9, .4), c(1, 0, 0, 1, 1))
 evaluate_classification <- function(predicted, actual) {
   c("AUPR" = MLmetrics::PRAUC(y_pred = predicted, y_true = actual),
     "AUROC" = MLmetrics::AUC(y_pred = predicted, y_true = actual)
   )
 }
 
+#' Get performance metrics for regression predictions
+#'
+#' @param predicted Vector of predicted values
+#' @param actual Vector of realized values
+#'
+#' @importFrom MLmetrics RMSE
+#' @importFrom MLmetrics MAE
+#' @importFrom MLmetrics R2_Score
+#'
+#' @return Numeric vector of scores with metric as names
+#' @export
+#'
+#' @examples
+#' evaluate_regression(c(2, 4, 6), c(1.5, 4.1, 6.2))
 evaluate_regression <- function(predicted, actual) {
-
+  c("RMSE" = MLmetrics::RMSE(y_pred = predicted, y_true = actual),
+    "MAE" = MLmetrics::MAE(y_pred = predicted, y_true = actual),
+    "Rsquared" = MLmetrics::R2_Score(y_pred = predicted, y_true = actual)
+  )
 }
-
