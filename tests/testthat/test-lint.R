49acda488954ac1dfdaa2847eda3911cd60b8185
Michael Levy <michael.levy@healthcatalyst.com>
2018-04-12 14:45:30 -0600

    testing lintr bot

--- tests/testthat/test-lint.R
+++ tests/testthat/test-lint.R
@@ -2,6 +2,9 @@ context("lints")
 test_that("Package is lint free", {
   skip_on_appveyor()
   skip_on_travis()
+  ((
+    # x <- 2 # Here are some lints.
+  ))
   lintr::expect_lint_free(
     # Anything changed here must be changed in .lintr too.
     linters = lintr::with_defaults(camel_case_linter = NULL,
