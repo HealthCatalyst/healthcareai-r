b626839ce44e6953ce27b4f011df8cb4939af46b
Mike Mastanduno <michael.mastanduno@healthcatalyst.com>
2018-04-18 19:28:57 -0400

    updated globals

--- R/globals.R
+++ R/globals.R
@@ -3,7 +3,9 @@
 utils::globalVariables(c("outside", "percent_missing", "variable", ".outcome",
                          ".", "Y", "twoClassSummary", "rowIndex",
                          "importance", "caret", "w_cc", "w_ccmcc",
-                         "fill_ones"))
+                         "fill_ones", "guessed_formats", "has_guesses",
+                         "first_entry", "converted_date", "is_valid",
+                         "any_valid"))
 
 printer <- utils::getFromNamespace("printer", "recipes")
 
