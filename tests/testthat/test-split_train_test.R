a8eb80c342889c92f22e1f6cebfe89425d612135
glenrs <grexsumsion@gmail.com>
2018-05-17 01:19:51 -0600

    testing for grouping

--- tests/testthat/test-split_train_test.R
+++ tests/testthat/test-split_train_test.R
@@ -1,3 +1,5 @@
+context("test split_train_test.R")
+
 data(mtcars)
 test_that("split train test returns a named list with two data frames", {
   sp <- split_train_test(mtcars, am, .8)
@@ -11,4 +13,45 @@ test_that("split train test respects seed", {
   sp1 <- split_train_test(mtcars, am, .8, 123)
   sp2 <- split_train_test(mtcars, am, .8, 123)
   expect_equal(sp1, sp2)
+})
+
+test_that("split train test grouping", {
+  owner <- c(
+    'a',
+    'a',
+    'b',
+    'b',
+    'c',
+    'c',
+    'd',
+    'd',
+    'e',
+    'e',
+    'f',
+    'f',
+    'g',
+    'g',
+    'h',
+    'h',
+    'i',
+    'i',
+    'j',
+    'j',
+    'k',
+    'k',
+    'l',
+    'l',
+    'm',
+    'm',
+    'n',
+    'n',
+    'o',
+    'o',
+    'p',
+    'p'
+  )
+  mtcars <- cbind(mtcars,owner)
+  sp1 <- split_train_test(mtcars, mpg, .75, 123, owner)
+  result <- length(intersect(sp1[[1]]$owner,sp1[[2]]$owner)) == 0
+  expect_true(length(intersect(sp1[[1]]$owner,sp1[[2]]$owner)) == 0)
 })
