decb3c85e174350b636ba7e125565832c8516f92
glenrs <grexsumsion@gmail.com>
2018-05-17 01:19:37 -0600

    grouping on spliting data into test and training

--- R/split_train_test.R
+++ R/split_train_test.R
@@ -6,15 +6,20 @@
 #' @param percent_train Proportion of rows in d to put into training. Default is 0.8
 #' @param seed Optional, if provided the function will return the same split
 #'   each time it is called
+#' @param grouping_col column header of dataframe to base grouping split
 #'
 #' @return A list of two data frames with names train and test
 #' @export
 #'
-#' @details This function wraps `caret::createDataPartition`.
+#' @details This function wraps `caret::createDataPartition`. If outcome is a factor
+#'   then the test/training porportions are stratified. Otherwise they are randomly 
+#'   selected. If the grouping_col is given, then groups are kept together where possible.
+#'   If grouping is prefered over stratified split, then make sure that outcome is
+#'   not a factor.
 #'
 #' @examples
 #' split_train_test(mtcars, am, .9)
-split_train_test <- function(d, outcome, percent_train = .8, seed) {
+split_train_test <- function(d, outcome, percent_train = .8, seed, grouping_col) {
   outcome <- rlang::enquo(outcome)
   if (rlang::quo_is_missing(outcome))
     stop("You must provide an outcome variable to tune_models.")
@@ -23,7 +28,14 @@ split_train_test <- function(d, outcome, percent_train = .8, seed) {
     stop(outcome_chr, " isn't a column in d.")
   if (!missing(seed))
     set.seed(seed)
-  train_rows <- caret::createDataPartition(dplyr::pull(d, !!outcome),
-                                           p = percent_train)[[1]]
-  list(train = d[train_rows, ], test = d[-train_rows, ])
+  if (!missing(grouping_col)){
+    train_rows <- sample.split(dplyr::pull(d, !!outcome),
+                               SplitRatio = percent_train,
+                               group = grouping_col)
+    return(list(train = d[train_rows, ], test = d[!train_rows, ]))
+  } else {
+    train_rows <- caret::createDataPartition(dplyr::pull(d, !!outcome),
+                                             p = percent_train)[[1]]
+    return(list(train = d[train_rows, ], test = d[-train_rows, ]))
+  }
 }
