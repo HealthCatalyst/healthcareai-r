caf2b4232ba3f43f6674bcc1cb6dc8d6ab27d7b8
Michael Levy <michael.levy@healthcatalyst.com>
2018-06-12 15:56:00 -0600

    add methods to depends in description

--- R/zzz.R
+++ R/zzz.R
@@ -3,5 +3,4 @@
                 "\nPlease visit https://docs.healthcare.ai for full documentation ",
                 "and vignettes. Join the community at https://healthcare-ai.slack.com")
   packageStartupMessage(mes)
-  library(methods)
 }
