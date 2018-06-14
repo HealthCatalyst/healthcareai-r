d1a137d901a4f1b6ffdd0f8d1302baee3b14bded
glenrs <grexsumsion@gmail.com>
2018-06-14 12:09:58 -0600

    wrote grouping_stratified_split function

--- R/split_train_test.R
+++ R/split_train_test.R
@@ -6,6 +6,10 @@
 #' @param percent_train Proportion of rows in d to put into training. Default is 0.8
 #' @param seed Optional, if provided the function will return the same split
 #'   each time it is called
+#' @param grouping_col column name that specifies grouping. Individuals in the same
+#'   group are in the same training/test set.
+#' @param aggreg_func when the data is grouped, the outcome column is aggregated
+#'   based on this function.
 #'
 #' @return A list of two data frames with names train and test
 #' @export
@@ -18,7 +22,7 @@
 #'
 #' @examples
 #' split_train_test(mtcars, am, .9)
-split_train_test <- function(d, outcome, percent_train = .8, seed) {
+split_train_test <- function(d, outcome, percent_train = .8, seed, grouping_col = NULL, aggreg_func = dplyr::first) {
   outcome <- rlang::enquo(outcome)
   if (rlang::quo_is_missing(outcome))
     stop("You must provide an outcome variable to tune_models.")
@@ -27,14 +31,29 @@ split_train_test <- function(d, outcome, percent_train = .8, seed) {
     stop(outcome_chr, " isn't a column in d.")
   if (!missing(seed))
     set.seed(seed)
-  # if (!missing(grouping_col)){
-  #   train_rows <- sample.split(dplyr::pull(d, !!outcome),
-  #                              SplitRatio = percent_train,
-  #                              group = grouping_col)
-  #   return(list(train = d[train_rows, ], test = d[!train_rows, ]))
-  # } else {
-  train_rows <- caret::createDataPartition(dplyr::pull(d, !!outcome),
-                                            p = percent_train)[[1]]
-  list(train = d[train_rows, ], test = d[-train_rows, ])
-  # }
+  if (!missing(grouping_col)){
+    return(
+      grouping_stratified_split(d, outcome, percent_train, grouping_col, aggreg_func)
+    )
+  } else {
+    train_rows <- caret::createDataPartition(dplyr::pull(d, !!outcome),
+                                             p = percent_train)[[1]]
+    return(list(train = d[train_rows, ], test = d[-train_rows, ]))
+  }
+}
+
+grouping_stratified_split <- function(d, outcome, percent_train = .8, grouping_col, aggreg_func = dplyr::first) {
+  #grouping_col <- rlang::enquo(grouping_col)
+  #outcome <- rlang::enquo(outcome)
+  d_limited <- d %>% dplyr::group_by(!!grouping_col) %>% dplyr::summarize(outcome = aggreg_func(!!outcome))
+
+  outcome <- rlang::quo(outcome)
+  col_insterest <- d_limited %>% dplyr::pull(!!outcome)
+
+  train_rows <- caret::createDataPartition(col_insterest, p = percent_train)[[1]]
+  group_training <- d_limited[train_rows, ] %>% dplyr::pull(!!grouping_col)
+
+  training_data <- dplyr::filter(d, !!grouping_col %in% group_training)
+  test_data <- dplyr::filter(d, !(!!grouping_col %in% group_training))
+  return(list(train = training_data, test = test_data))
 }
