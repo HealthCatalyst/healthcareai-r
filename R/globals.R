684f0e490b33f0df2895e8b22b9a518bd5df29bf
Michael Levy <michael.levy@healthcatalyst.com>
2018-07-26 16:42:29 -0600

    rebalance tests for travis timing, add globals

--- R/globals.R
+++ R/globals.R
@@ -8,8 +8,11 @@ utils::globalVariables(c("outside", "percent_missing", "variable", ".outcome",
                          "any_valid", "badness", "best_levels", "fraction_positive",
                          "log_dist_from_in_all", "log_loss", "mean_ssd",
                          "predictor_of", "present_in", "original_data_str",
-                         "coefficient", "model", "obs"))
-
+                         "coefficient", "model", "obs", "alt_value",
+                         "base_prediction", "current_value", "from", "impact_rank",
+                         "improvement", "measure", "new_prediction", "optimal",
+                         "predict", "process_variable_name", "row_id",
+                         "threshold", "to", "value"))
 
 printer <- utils::getFromNamespace("printer", "recipes")
 
