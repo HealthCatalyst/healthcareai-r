eebaf1a82f3c5f5dfb0056b000ee1447a2681b01
Rex Sumsion <rex.sumsion@healthcatalyst.com>
2018-08-23 15:16:25 -0600

    resolve merge conflicts

--- R/globals.R
+++ R/globals.R
@@ -12,11 +12,8 @@ utils::globalVariables(c("outside", "percent_missing", "variable", ".outcome",
                          "base_prediction", "current_value", "from", "impact_rank",
                          "improvement", "measure", "new_prediction", "optimal",
                          "predict", "process_variable_name", "row_id",
-<<<<<<< HEAD
-                         "threshold", "to", "value", "reference_level"))
-=======
-                         "threshold", "to", "value", "quantiles", "predicted_group"))
->>>>>>> master
+                         "threshold", "to", "value", "quantiles",
+                         "predicted_group", "reference_level"))
 
 printer <- utils::getFromNamespace("printer", "recipes")
 
