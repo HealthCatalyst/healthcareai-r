7e560d3eaaba32bafb260ccdc871a76000f7060f
Rex Sumsion <rex.sumsion@healthcatalyst.com>
2018-08-30 09:00:29 -0600

    linting

--- tests/testthat/test-training_setup.R
+++ tests/testthat/test-training_setup.R
@@ -1,6 +1,6 @@
 context("checking training setup")
 
-test_that("check_metric when correct, doesn't throw error or change metric" , {
+test_that("check_metric when correct, doesn't throw error or change metric", {
   expect_warning(metric <- check_metric("classification", "PR"), NA)
   expect_equal(metric, "PR")
 
@@ -8,7 +8,7 @@ test_that("check_metric when correct, doesn't throw error or change metric" , {
   expect_equal(metric, "RMSE")
 })
 
-test_that("check_metric when incorrect, throws error or throws warnings and corrects" , {
+test_that("check_metric when incorrect, throws error or throws warnings and corrects", {
   expect_warning(new_metric <- check_metric("classification", "MAE"))
   expect_equal(new_metric, "ROC")
 
