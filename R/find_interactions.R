b728cd39da90ad3186fae4c1050a93f0e039bd90
Rex Sumsion <rex.sumsion@healthcatalyst.com>
2018-11-16 00:12:45 -0700

    finalize find_interactions

---
+++ R/find_interactions.R
@@ -0,0 +1,198 @@
+#' Calculates the h-statistic for either all combinations or for the most
+#' significant interactions.
+#' @param d Dataframe or tibble
+#' @param outcome Unquoted column name that indicates the target variable.
+#' @param brute_force Logical. When TRUE (default) every combination of
+#'   variables will be searched. If FALSE, a greedy implementation will be used.
+#'   If greedy is used hidden interactions will not be found.
+#' @param order Integer (default = 3) specifying how many orders to search for
+#'   in the data set. This function is designed for 2 or 3 orders.
+#' @param random_seed Numeric. Sets the random seed for the gbm model. If not
+#'   provided, models will be randomized.
+#' @param verbose Logical. If TRUE (default) will list the amount of time it
+#'   takes to run brute_force.
+#' @param greedy_cutoff Numeric. If brute_force is FALSE, all H-statistics above
+#'   this threshold will not be explored for further interactions.
+#'
+#' @details get_interactions is dependent an tidyverse, gbm, recipes, and rlang.
+#'   This does not support data columns of type character or date. This function
+#'   also doesn't support columns that only contain missing values. Currently
+#'   supports regression and binomial output.
+#' @export
+find_interactions <- function(d, outcome, brute_force = TRUE, order = 3,
+                              random_seed, verbose = TRUE, greedy_cutoff = .05) {
+  quo_outcome <- rlang::enquo(outcome)
+
+  # Make the gbm model
+  formula_str <- paste0(rlang::quo_name(quo_outcome), " ~ .")
+
+  # Get outcome type
+  outcome_col <- pull(d, !!rlang::quo_name(quo_outcome))
+  n_distinct <- n_distinct(outcome_col)
+  if (n_distinct == 2) {
+    if (verbose)
+      message("Assuming bernoulli from outcome variable")
+    if (class(outcome_col) == "factor") {
+      d <-
+        d %>%
+        mutate(!!rlang::quo_name(quo_outcome) := as.numeric(!!quo_outcome) - 1)
+    } else if (class(outcome_col) == "logical") {
+      d <-
+        d %>%
+        mutate(!!rlang::quo_name(quo_outcome) := as.numeric(!!quo_outcome))
+    }
+    distribution <- "bernoulli"
+  } else {
+    if (class(outcome_col) == "factor") {
+      stop("Find interactions doesn't support multiclass outcome variables")
+    } else {
+      if (verbose)
+        message("Assuming gaussian from outcome variable")
+      distribution <- "gaussian"
+    }
+  }
+
+
+  if (!missing(random_seed))
+    set.seed(random_seed)
+  m <- gbm(as.formula(formula_str), d, interaction.depth = order,
+           distribution = distribution)
+
+  if (brute_force) {
+    brute_force_interactions(d, quo_outcome, m, order, verbose, random_seed)
+  } else {
+    important_features <- rownames(summary(m, plotit = FALSE))[summary(m, plotit = FALSE)$rel.inf != 0]
+    interactions <- list(combinations = list(), significance = c())
+    starting_order <- 2 # H-statistic starts at 2nd order interactions
+    greedy_interactions(d, m, map(important_features, ~{.x}),
+                        important_features, interactions, starting_order, order,
+                        greedy_cutoff)
+  }
+}
+
+#' Estimates the amount of time it will take to calculate all interactions to
+#' a specific order
+#' @noRd
+estimate_time <- function(n_variables, interact_time, order) {
+  n_iterations <- 0
+  for (i in 2:order) {
+    n_iterations <- n_iterations + choose(n_variables, i)
+  }
+  return(n_iterations*interact_time)
+}
+
+#' Calculates the H statistic for evey possible interaction of the order given
+#' @noRd
+brute_force_interactions <- function(d, quo_outcome, m, order, verbose,
+                                     random_seed) {
+  # find all the independent variable names
+  indepedent_vars <-
+    select(d, -!!quo_outcome) %>%
+    names()
+
+  if (verbose) {
+    begin <- proc.time()
+    interact.gbm(m, d, i.var = indepedent_vars[1:2])
+    end <- proc.time() - begin
+
+    est <- estimate_time(length(indepedent_vars), end[3], order)
+    message("This will take ", est, "seconds")
+  }
+
+  # We only want to calculate up to 4th order interactions
+  order_limit <-
+    if (length(indepedent_vars) > order)
+      order
+  else
+    length(indepedent_vars)
+
+  # fina all combinations from 2nd to 4th order interactions
+  combn_vars = list()
+  for (i in 2:order_limit) {
+    combn_vars <- c(combn_vars, combn(indepedent_vars, i, simplify = FALSE))
+  }
+
+  sig <- map_dbl(combn_vars, ~{
+    interact.gbm(m, d, i.var = .x)
+  })
+  list(combinations = combn_vars, significance = sig)
+}
+
+#' Calculates the H statistic for interactions that are related to lower
+#' significant interactions. This follows the process of Friedman.
+#' @noRd
+greedy_interactions <- function(data, gbm_model, testing_interactions,
+                                important_features, interactions, cur_order,
+                                order_limit, greedy_cutoff) {
+  if (cur_order <= order_limit) {
+    for (i in 1:length(testing_interactions)) {
+      interactions <- greedy_interactions_recursion(data, gbm_model,
+                                              testing_interactions[[i]],
+                                              important_features,
+                                              interactions, cur_order,
+                                              order_limit, greedy_cutoff)
+    }
+  }
+  return(interactions)
+}
+
+#' Called by greedy_interactions as a helper in recursion
+#' @noRd
+greedy_interactions_recursion <- function(data, gbm_model, testing_interaction,
+                                    important_features, interactions,
+                                    cur_order, order_limit, greedy_cutoff) {
+  interaction_significance <- NULL
+  interaction_combinations <- list()
+  interested <- NULL
+  for (i in 1:length(important_features)) {
+    # get the interaction significance for each combination of variables, i.var
+    # finds the h-statistic for interaction significance for all items given
+    interaction_combinations[[i]] <- c(testing_interaction,
+                                       important_features[i])
+    interaction_combinations[[i]] <- interaction_combinations[[i]][
+      order(interaction_combinations[[i]])
+      ]
+
+    #
+    if (important_features[i] %in% testing_interaction) {
+      interested[i] <- FALSE
+      interaction_significance[i] <- 0
+      next
+    }
+
+    interaction_significance[i] <-
+      gbm::interact.gbm(gbm_model, data,
+                        i.var = c(interaction_combinations[[i]]))
+
+
+    interested[i] <- interaction_significance[i] > greedy_cutoff
+
+
+    # Remove the interaction if it is reflexive
+    if (length(interactions$combinations) > 0)
+      if (interested[i] & any(map_lgl(interactions$combinations, ~{
+        identical(.x, interaction_combinations[[i]])
+      })))
+        interested[i] <- FALSE
+
+  }
+
+  # Reorder combinantions to find matches in the future
+  interactions$combinations <- c(interactions$combinations,
+                                 interaction_combinations[interested])
+  interactions$significance <- c(interactions$significance,
+                                 interaction_significance[interested])
+
+  # Only continue recursion if there are no more possible interactions. If there
+  # is only one, it might find an interaction with itself when we switch to
+  # a tree based function.
+  if (sum(interested) > 1) {
+    interactions <- greedy_interactions(data, gbm_model,
+                                     interaction_combinations[interested],
+                                     important_features[interested],
+                                     interactions, cur_order + 1, order_limit,
+                                     greedy_cutoff)
+  }
+
+  return(interactions)
+}
---
+++ tests/testthat/test-find_interactions.R
@@ -0,0 +1,113 @@
+context("Checking test-find_interactions.R")
+
+# Setup ------------------------------------------------------------------------
+d <-
+  pima_diabetes %>%
+  slice(1:100) %>%
+  mutate_if(is.character, as.factor)
+
+d_2vars <-
+  d %>%
+  select(age, diabetes, weight_class)
+
+# Test missing_check -----------------------------------------------------------
+test_that("test regression output", {
+  expect_message(
+    interactions <- find_interactions(select(d, pregnancies, age, weight_class),
+                                    outcome = pregnancies),
+    "gaussian"
+  )
+  expect_true(interactions$significance[1] > 0)
+})
+
+test_that("test binomial output, converting factor to numeric and logical to numeric", {
+  expect_message(
+    interactions <- find_interactions(d_2vars, outcome = diabetes),
+    "bernoulli"
+  )
+  expect_true(interactions$significance[1] > 0)
+  interactions <- find_interactions(mutate(d_2vars, diabetes = diabetes == "Y"), outcome = diabetes)
+  expect_true(interactions$significance[1] > 0)
+})
+
+test_that("doesn't crash with missing values", {
+  interactions <- find_interactions(d_2vars, outcome = diabetes)
+  expect_true(interactions$significance[1] > 0)
+})
+
+test_that("random seed works", {
+
+  interactions_1 <- find_interactions(d_2vars, outcome = diabetes)
+  interactions_2 <- find_interactions(d_2vars, outcome = diabetes)
+  expect_true(round(interactions_1$significance[1], 2) != round(interactions_2$significance[1], 2))
+
+  interactions_1 <- find_interactions(d_2vars, outcome = diabetes, random_seed = 1)
+  interactions_2 <- find_interactions(d_2vars, outcome = diabetes, random_seed = 1)
+  # There is still some randomness, but it should be small
+  expect_equal(round(interactions_1$significance[1], 2),
+               round(interactions_2$significance[1], 2))
+})
+
+test_that("test verbos", {
+  expect_message(
+    find_interactions(d_2vars, outcome = diabetes),
+    "This will take"
+  )
+
+  # No message should be delivered if verbose is silenced.
+  mes <- capture_message(
+    find_interactions(d_2vars, outcome = diabetes, verbose = FALSE)
+  )
+  expect_true(is.null(mes))
+
+  # No message should be delivered if using brute force
+  mes <- capture_message(
+    find_interactions(d_2vars, outcome = diabetes, brute_force = FALSE)
+  )
+  expect_false(str_detect(paste(mes), "This will take"))
+})
+
+test_that("test brute force", {
+  interactions <- find_interactions(d, outcome = diabetes)
+  n_possible <- choose(length(d) - 1, 2) + choose(length(d) - 1, 3)
+  expect_equal(length(interactions$significance), n_possible)
+
+  interactions <- find_interactions(d, outcome = diabetes, order = 2)
+  n_possible <- choose(length(d) - 1, 2)
+  expect_equal(length(interactions$significance), n_possible)
+  expect_equal(length(interactions$significance), length(interactions$combinations))
+})
+
+test_that("test greedy", {
+  # Greedy should get less interactions when significance is .05
+  interactions <- find_interactions(d, outcome = diabetes, brute_force = FALSE)
+  n_possible <- choose(length(d) - 1, 2) + choose(length(d) - 1, 3)
+  expect_true(length(interactions$significance) < n_possible)
+
+  # When the cutoff is negative we should get all the interactions
+  interactions <- find_interactions(d, outcome = diabetes, brute_force = FALSE,
+                                   greedy_cutoff = -1)
+  n_possible <- choose(length(d) - 1, 2) + choose(length(d) - 1, 3)
+  expect_true(length(interactions$significance) == n_possible)
+
+  # Test order change to 2nd order, when the cutoff is negative we should get
+  # all the interactions
+  interactions <- find_interactions(d, outcome = diabetes, brute_force = FALSE,
+                                   greedy_cutoff = -1, order = 4)
+  n_possible <- choose(length(d) - 1, 2) + choose(length(d) - 1, 3) +
+    choose(length(d) - 1, 4)
+  expect_true(length(interactions$significance) == n_possible)
+  expect_equal(length(interactions$significance), length(interactions$combinations))
+})
+
+test_that("test estimate_time", {
+  estimate <- estimate_time(5, 1, 2)
+  actual <- factorial(5)/(2*3*2)
+  expect_equal(estimate, actual)
+
+  estimate <- estimate_time(5, 1, 3)
+  actual <- factorial(5)/(2*3*2) + factorial(5)/(2*3*2)
+  expect_equal(estimate, actual)
+})
+
+
